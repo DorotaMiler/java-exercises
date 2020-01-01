3815e49c5d230c91f384a179cc656f8cc8f901a0
Dorota Miler <dorota.miler@outlook.com>
2020-01-01 21:11:53 +0100

    organizing contents of project locally

---
+++ src/main/java/_1_HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+package _1_HelloWorld;
+
+/**
+ * this type of multi-line comment is used in program's documentation creation
+ HelloWorld1 java class addition
+ @param args - parameters passed after the program is run
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) { // a starting method
+        System.out.println("Hello world again :)!"); // Display the first String
+        System.out.println("Take care and all the best!");  // Display the second String
+    }
+}
