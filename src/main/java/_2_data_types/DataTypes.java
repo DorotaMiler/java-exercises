8aa9e876c958680885006fdf335cd6abfce7dddb
Dorota Miler <dorota.miler@outlook.com>
2020-01-01 21:19:25 +0100

    addition of new package DataTypes + DatatTypes.java

---
+++ src/main/java/_2_data_types/DataTypes.java
@@ -0,0 +1,18 @@
+package _2_data_types;
+
+/** practice of utilizing
+ * different data types in JAVA
+ */
+
+public class DataTypes{
+    public static void main(String[] args) {
+        System.out.println("A printout of exemplary data types: \n");
+        System.out.println('a'+'A'); // char
+        System.out.println(1+4);  // f.ex. byte
+        System.out.println(1.0+2.0); // float
+        System.out.println("quatation mark \""); // \"
+        System.out.println("apostrophe \'");  // \'
+        System.out.println("backslash \\");  // \\
+        System.out.println("new line \n");  // \n
+    }
+}
