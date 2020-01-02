d505efd9d774ff55b9cc0b46374619da7e946280
Dorota Miler <dorota.miler@outlook.com>
2020-01-02 20:39:29 +0100

    add comments in Variables.java

--- src/main/java/_3_variables/Variables.java
+++ src/main/java/_3_variables/Variables.java
@@ -2,8 +2,11 @@ package _3_variables;
 
 public class Variables {
     public static void main(String[] rgs){
-        int age;  //  variable declaration
-        age = 19; // variable initiation
+        int age;  //  variable declaration (variable's type choice + it's name)
+        age = 19; // afterwards variable initiation
+        /* most common to concatenate both in one row:
+        int age = 19;
+         */
         System.out.println("Mam "+age+" lat");
         System.out.println(age+" lat to piekny wiek..");
         System.out.println("Dodatkowo "+age+" to liczba pierwsza");
