f27ad5ec574aec981914137041b1532b9564c937
Dorota Miler <dorota.miler@outlook.com>
2020-01-02 21:37:21 +0100

    Operators.java simple mathematical calculations

--- src/main/java/_4_math_and_logic_operators/Operators.java
+++ src/main/java/_4_math_and_logic_operators/Operators.java
@@ -1,4 +1,18 @@
 package _4_math_and_logic_operators;
 
 public class Operators {
+    public static void main(String[] args) {
+        int a = 17;
+        int b = 4;
+        int c = a + b;  // 21
+        System.out.println(c);
+        c = a - b;  // 13
+        System.out.println(c);
+        c = a * b;  // 68
+        System.out.println(c);
+        c = a / b;  // 4 cause 4 * 4 = 16 and remaining rest = 1;
+        System.out.println(c);
+        c = a % b;
+        System.out.println(c);
+    }
 }
