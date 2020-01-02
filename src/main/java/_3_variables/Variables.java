d36bac64d4eabbab6d8264f39d0bf95b7e96b486
Dorota Miler <dorota.miler@outlook.com>
2020-01-02 20:33:56 +0100

    add exemplary Variable.java contents

--- src/main/java/_3_variables/Variables.java
+++ src/main/java/_3_variables/Variables.java
@@ -1,4 +1,12 @@
 package _3_variables;
 
 public class Variables {
+    public static void main(String[] rgs){
+        int age;  //  variable declaration
+        age = 19; // variable initiation
+        System.out.println("Mam "+age+" lat");
+        System.out.println(age+" lat to piekny wiek..");
+        System.out.println("Dodatkowo "+age+" to liczba pierwsza");
+        System.out.println("Liczba, ktora nastepuje po "+age+" to: "+(age+1));
+    }
 }
