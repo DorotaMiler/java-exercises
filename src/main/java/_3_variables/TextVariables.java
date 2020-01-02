62db0f8452201d007cc98f9b87568e4a7eab2bf3
Dorota Miler <dorota.miler@outlook.com>
2020-01-02 21:20:30 +0100

    add substring() function

--- src/main/java/_3_variables/TextVariables.java
+++ src/main/java/_3_variables/TextVariables.java
@@ -1,4 +1,13 @@
 package _3_variables;
 
 public class TextVariables {
+    public static void main(String[] args) {
+        String hello = "Hello ";
+        String world = "world";
+        String greetings = hello + world;  //  concatenation of 2 texts
+        System.out.println(greetings);
+
+        String helloStudent = greetings.substring(0,6)+"student";
+        System.out.println(helloStudent);
+    }
 }
