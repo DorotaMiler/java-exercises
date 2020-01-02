d08e19b0da57c1a138a4cd1c6e24e619a60a9edd
Dorota Miler <dorota.miler@outlook.com>
2020-01-02 20:55:23 +0100

    2 integers exemplary calculation

--- src/main/java/_3_variables/Calculator.java
+++ src/main/java/_3_variables/Calculator.java
@@ -1,4 +1,11 @@
 package _3_variables;
 
 public class Calculator {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        System.out.println("a = b = "+(a+b));
+
+        
+    }
 }
