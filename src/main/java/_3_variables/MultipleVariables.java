9fbda5f8c4f484665d74ceb80f189b5d203294e4
Dorota Miler <dorota.miler@outlook.com>
2020-01-02 20:49:45 +0100

    add MultipleVariables.java

---
+++ src/main/java/_3_variables/MultipleVariables.java
@@ -0,0 +1,17 @@
+package _3_variables;
+
+public class MultipleVariables {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = number1;
+        System.out.println("Sum of number1 & number2 = "+(number1+number2)); // 10
+        number1=10;
+        System.out.println("number2 = "+number2); // 5
+
+        double number3, number4;
+        number3 = 5.0;
+        number4 = 3.3;
+        System.out.println("number3 = "+number3);  // 5.0
+        System.out.println("number4 = "+number4);  // 3.3
+    }
+}
