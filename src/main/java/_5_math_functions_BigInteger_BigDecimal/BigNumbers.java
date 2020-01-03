32a9f35e247af1323a26f1f85cc18f28eac5831b
Dorota Miler <dorota.miler@outlook.com>
2020-01-03 21:45:58 +0100

    BigNumbers.java BigDecimal example with divide() & multiply() usage

--- src/main/java/_5_math_functions_BigInteger_BigDecimal/BigNumbers.java
+++ src/main/java/_5_math_functions_BigInteger_BigDecimal/BigNumbers.java
@@ -9,6 +9,7 @@ BigInteger.ONE / BigDecimal.ONE
 BigInteger.ZERO / BigDecimal.ZERO
  */
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 
 public class BigNumbers {
@@ -23,11 +24,15 @@ public class BigNumbers {
         //  BigInteger bigNumber = BigInteger.valueOf(1234567890)
         System.out.println("bigNumber = "+bigNumber);
 
-        // add() substract(), multiply(), devide()
+        // add() substract(), multiply(), divide()
         BigInteger first = new BigInteger("123456789012345678901234567890");
         BigInteger second = new BigInteger("987654321987654321987654321");
         BigInteger sum = first.add(second);
         System.out.println("Sum of BigIntegers first: "+first+" and second: "+second+" = "+sum.toString());
 
+        BigDecimal a = new BigDecimal("9.5");
+        BigDecimal b = new BigDecimal("4");
+        BigDecimal c = new BigDecimal("0.33");
+        System.out.println("Calculation og BigDecimals a, b & c = "+a.divide(b).multiply(c));
     }
 }
