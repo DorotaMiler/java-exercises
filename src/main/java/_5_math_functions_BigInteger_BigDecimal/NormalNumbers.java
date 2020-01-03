427a1c828ec014ff228299e803907f8ae04b86c6
Dorota Miler <dorota.miler@outlook.com>
2020-01-03 21:40:51 +0100

    NormalNumbers.java comment addition

--- src/main/java/_5_math_functions_BigInteger_BigDecimal/NormalNumbers.java
+++ src/main/java/_5_math_functions_BigInteger_BigDecimal/NormalNumbers.java
@@ -1,4 +1,11 @@
 package _5_math_functions_BigInteger_BigDecimal;
 
 public class NormalNumbers {
+    public static void main(String[] args) {
+        double a = 9.5;
+        double b = 4;
+        double c = 0.33;
+        System.out.println("Calculation of a, b & c = "+(a/b*c)); // not precise result due
+        // to a fact that doubles are only rounded numbers
+    }
 }
