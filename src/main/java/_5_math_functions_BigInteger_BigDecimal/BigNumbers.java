8bf4c117eeeab088ffde421515c9dfa3b176d4aa
Dorota Miler <dorota.miler@outlook.com>
2020-01-03 21:22:00 +0100

    BigNumbers.java creation

--- src/main/java/_5_math_functions_BigInteger_BigDecimal/BigNumbers.java
+++ src/main/java/_5_math_functions_BigInteger_BigDecimal/BigNumbers.java
@@ -1,4 +1,19 @@
 package _5_math_functions_BigInteger_BigDecimal;
 
+/*
+BigInteger
+BigDecimal
+
+useful constants are concluded:
+BigInteger.ONE / BigDecimal.ONE
+BigInteger.ZERO / BigDecimal.ZERO
+ */
+
+import java.math.BigInteger;
+
 public class BigNumbers {
+    public static void main(String[] args) {
+        BigInteger bigNumber = new BigInteger("123");
+        System.out.println("bigNumber = "+bigNumber);
+    }
 }
