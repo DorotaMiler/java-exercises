0c733f08aef40d98ea9c5d63b59e515a55d384cc
Dorota Miler <dorota.miler@outlook.com>
2020-01-03 19:12:49 +0100

    MathFunctions.java -> sqrt & power example

--- src/main/java/_5_math_functions_BigInteger_BigDecimal/MathFunctions.java
+++ src/main/java/_5_math_functions_BigInteger_BigDecimal/MathFunctions.java
@@ -1,10 +1,22 @@
 package _5_math_functions_BigInteger_BigDecimal;
 
+//  sqrt(double number)  -> radical (pierwiastek)
+//  pow(double a, double b)  -> power (potega) result = a powered to b
+//  abs(number)  -> absolute value of any number
+
+// more functions:  https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/Math.html
+
+/* class Math -> all methods are static (no Object required)
+Math.method_name(arguments)
+ */
 public class MathFunctions {
-    //  sqrt(double number)  -> radical (pierwiastek)
-    //  pow(double a, double b)  -> power (potega) result = a powered to b
-    //  abs(number)  -> absolute value of any number
+    public static void main(String[] args) {
+        double a = 9.0;
+        int b = 3;
+        double sqrt = Math.sqrt(a);
+        double power = Math.pow(a, b);
 
-    // more functions:  https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/Math.html
-
+        System.out.println("radical from "+a+" = "+sqrt);
+        System.out.println("Number "+a+" powered to "+b+" = "+power);
+    }
 }
