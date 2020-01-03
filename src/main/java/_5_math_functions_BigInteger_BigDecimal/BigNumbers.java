20c87a08bba01fd2ffc4293a8a840e38ad1fb4f6
Dorota Miler <dorota.miler@outlook.com>
2020-01-03 21:25:34 +0100

    BigNumbers.java comments addition

--- src/main/java/_5_math_functions_BigInteger_BigDecimal/BigNumbers.java
+++ src/main/java/_5_math_functions_BigInteger_BigDecimal/BigNumbers.java
@@ -13,7 +13,11 @@ import java.math.BigInteger;
 
 public class BigNumbers {
     public static void main(String[] args) {
-        BigInteger bigNumber = new BigInteger("123");
+        BigInteger bigNumber = new BigInteger("123");  /* argument as a String variable type
+                                                              constructor accepts String, not int or long
+                                                              */
+        //  another way to create object:
+        //  BigInteger bigNumber = BigInteger.valueOf(1234567890)
         System.out.println("bigNumber = "+bigNumber);
     }
 }
