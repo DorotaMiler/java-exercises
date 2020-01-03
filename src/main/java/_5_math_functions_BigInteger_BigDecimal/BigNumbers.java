c2844c20e6daac046031e8b28d9cabeb3205f2d3
Dorota Miler <dorota.miler@outlook.com>
2020-01-03 21:37:38 +0100

    BigNumbers.java add() method example, afterwards usage of  toString()

--- src/main/java/_5_math_functions_BigInteger_BigDecimal/BigNumbers.java
+++ src/main/java/_5_math_functions_BigInteger_BigDecimal/BigNumbers.java
@@ -17,7 +17,17 @@ public class BigNumbers {
                                                               constructor accepts String, not int or long
                                                               */
         //  another way to create object:
+        //  *important! (however to be careful not to exceed limit for long with BigInteger
+        //   and double with BigDecimal) - there is no such problem with
+        //  object creation and utilizing String argument as in the example above
         //  BigInteger bigNumber = BigInteger.valueOf(1234567890)
         System.out.println("bigNumber = "+bigNumber);
+
+        // add() substract(), multiply(), devide()
+        BigInteger first = new BigInteger("123456789012345678901234567890");
+        BigInteger second = new BigInteger("987654321987654321987654321");
+        BigInteger sum = first.add(second);
+        System.out.println("Sum of BigIntegers first: "+first+" and second: "+second+" = "+sum.toString());
+
     }
 }
