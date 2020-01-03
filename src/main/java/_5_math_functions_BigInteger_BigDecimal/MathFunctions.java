02a447b0412236619d2e6830a43488722bf1b94f
Dorota Miler <dorota.miler@outlook.com>
2020-01-03 19:00:18 +0100

    MathFunctions.java along with few patterns in comments

---
+++ src/main/java/_5_math_functions_BigInteger_BigDecimal/MathFunctions.java
@@ -0,0 +1,8 @@
+package _5_math_functions_BigInteger_BigDecimal;
+
+public class MathFunctions {
+    //  sqrt(double number)  -> radical (pierwiastek)
+    //  pow(double a, double b)  -> power (potega) result = a powered to b
+    //  abs(number)  -> absolute value of any number
+
+}
