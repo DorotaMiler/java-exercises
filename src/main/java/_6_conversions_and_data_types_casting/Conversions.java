d9af7cc55e9ee8d26a022bbb5827c5cb38a10c64
Dorota Miler <dorota.miler@outlook.com>
2020-01-05 17:34:34 +0100

    Conversions.java - automatic expanding conversion example

--- src/main/java/_6_conversions_and_data_types_casting/Conversions.java
+++ src/main/java/_6_conversions_and_data_types_casting/Conversions.java
@@ -1,4 +1,15 @@
 package _6_conversions_and_data_types_casting;
 
+/*
+(niejawna konwersja rozszerzajaca) - having doble devided by int
+
+ */
+
 public class Conversions {
+    public static void main(String[] args) {
+        int a = 5;   // before devision is done, a converted to double, therefore c MUST be double as well
+        double b = 13.5;
+        double c = b /a;  // if c was int, ERROR would occure
+        System.out.println("c = "+ c);
+    }
 }
