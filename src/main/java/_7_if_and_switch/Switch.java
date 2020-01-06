eed2bb84ef97e257ac626788e3df8ed39ef07536
Dorota Miler <dorota.miler@outlook.com>
2020-01-06 18:23:00 +0100

    Switch.java error corrected - got rid of do() while()

--- src/main/java/_7_if_and_switch/Switch.java
+++ src/main/java/_7_if_and_switch/Switch.java
@@ -13,22 +13,20 @@ public class Switch {
         Scanner scan = new Scanner(System.in);
         int choice = scan.nextInt();
 
-        do {
-            switch (choice) {
-                case 1:
-                    System.out.println("Options menu: ");
-                    break;  /* breas causes to stop the case block
+        switch (choice) {
+            case 1:
+                System.out.println("Options menu: ");
+                break;  /* breas causes to stop the case block
                 otherwise all below 3 options would be executed as well
                 */
-                case 2:
-                    System.out.println("Clients menu: ");
-                    break;
-                case 3:
-                    System.out.println("Addresses menu: ");
-                    break;
-                default:  // default not necessary; after default instructions in case the above were not met
-                    System.out.println("Please choose 1 of 4 available variants");
-            }
-        } while (choice != 4);
+            case 2:
+                System.out.println("Clients menu: ");
+                break;
+            case 3:
+                System.out.println("Addresses menu: ");
+                break;
+            default:  // default not necessary; after default instructions in case the above were not met
+                System.out.println("Please choose 1 of 4 available variants");
+        }
     }
 }
---
+++ src/main/java/_9_loops/Loop.java
@@ -0,0 +1,14 @@
+package _9_loops;
+
+public class Loop {
+    public static void main(String[] args) {
+        int counter = 0;
+
+        do {
+            System.out.println("This is a loop");
+            counter++;
+        }
+        while (counter < 10);
+        System.out.println("The end of loop");
+    }
+}
