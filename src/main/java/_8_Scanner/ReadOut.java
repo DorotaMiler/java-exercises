26930a843697b72bb7b446fa8ec4908d0ac9c3c2
Dorota Miler <dorota.miler@outlook.com>
2020-01-07 21:11:14 +0100

    ReadOut.java error corrected - txt file not foude tehrefore file.getAbsolutePath() utilized

--- src/main/java/_8_Scanner/ReadOut.java
+++ src/main/java/_8_Scanner/ReadOut.java
@@ -1,5 +1,6 @@
 package _8_Scanner;
 
+import javax.xml.bind.SchemaOutputResolver;
 import java.io.File;
 import java.util.Scanner;
 import java.io.FileNotFoundException;
@@ -7,7 +8,8 @@ import java.io.FileNotFoundException;
 public class ReadOut {
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("dorota.txt");
-        Scanner scan = new Scanner(file);
+        //System.out.println(file.getAbsolutePath());
+        Scanner scan = new Scanner(file.getAbsolutePath());
 
         String sentence = scan.nextLine();
         System.out.println("Read-out sentence is: " + sentence);
