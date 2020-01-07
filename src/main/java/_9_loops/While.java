2e7f088f758ea41fb5b4de314a0b0e5127f68be4
Dorota Miler <dorota.miler@outlook.com>
2020-01-07 23:18:16 +0100

    While.java with a loop

--- src/main/java/_9_loops/While.java
+++ src/main/java/_9_loops/While.java
@@ -1,4 +1,13 @@
 package _9_loops;
 
 public class While {
+    public static void main(String[] args) {
+        int counter = 0;
+
+        while (counter < 10) {
+            System.out.println("This is a loop");
+            counter++;
+        }
+        System.out.println("The end of loop");
+    }
 }
