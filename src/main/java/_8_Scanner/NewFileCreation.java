be5921fb2698235190c237074f7189191b779d92
Dorota Miler <dorota.miler@outlook.com>
2020-01-07 23:08:39 +0100

    NewFileCreation.java comments + created.txt adjusted due to PrintWriter

--- src/main/java/_8_Scanner/NewFileCreation.java
+++ src/main/java/_8_Scanner/NewFileCreation.java
@@ -2,12 +2,18 @@ package _8_Scanner;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class NewFileCreation {
     public static void main(String[] args) throws IOException {
         File file = new File("created.txt");
         boolean success = file.createNewFile();
-        System.out.println("Was file successfully created: "+success);
+        System.out.println("Was file successfully created: " + success);
 
+        PrintWriter writer = new PrintWriter(file);
+        writer.println("Luki ma loki :)");
+        writer.println("A Kubi i Fili to nasza miloc");
+        writer.close();    //  only after close() elements are written in a file;
+        //  before that all stored in buffer
     }
 }
