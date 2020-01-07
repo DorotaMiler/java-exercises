d506e2bc0d175ee00cd19b8344c9fe3c110e8283
Dorota Miler <dorota.miler@outlook.com>
2020-01-07 22:09:46 +0100

    created.txt creation and dragged into _8_Scanner package

--- src/main/java/_8_Scanner/NewFileCreation.java
+++ src/main/java/_8_Scanner/NewFileCreation.java
@@ -1,4 +1,13 @@
 package _8_Scanner;
 
+import java.io.File;
+import java.io.IOException;
+
 public class NewFileCreation {
+    public static void main(String[] args) throws IOException {
+        File file = new File("created.txt");
+        boolean success = file.createNewFile();
+        System.out.println("Was file successfully created: "+success);
+
+    }
 }
