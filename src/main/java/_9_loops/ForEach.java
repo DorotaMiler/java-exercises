504fadb39603d8576619fd9cf60f3bae3af4fc23
Dorota Miler <dorota.miler@outlook.com>
2020-01-08 20:58:25 +0100

    ForEach.java example

--- src/main/java/_9_loops/ForEach.java
+++ src/main/java/_9_loops/ForEach.java
@@ -3,5 +3,15 @@ package _9_loops;
 public class ForEach {
     public static void main(String[] args) {
 
+        // filling in table with numbers from1 to 10
+        int [] table = new int[10];
+        for (int i = 0; i <10; i++){
+            table[i] = i + 1;
+        }
+
+        // display of the created table
+        for (int x : table){
+            System.out.println(x + " element of the table = "+ x);
+        }
     }
 }
