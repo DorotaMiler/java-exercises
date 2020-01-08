29e44c87d984b7795fe4b6f3e269f7f83a253933
Dorota Miler <dorota.miler@outlook.com>
2020-01-08 21:14:37 +0100

    Table.java example

--- src/main/java/_10_tables/Table.java
+++ src/main/java/_10_tables/Table.java
@@ -1,4 +1,16 @@
 package _10_tables;
 
 public class Table {
+    public static void main(String[] args) {
+        int[] table = new int[20];
+        for (int i = 0; i < 20; i++) {
+            table[i] = i + 1;
+        }
+        int variable = table[3];
+        System.out.println("Chosen variable = " + variable);
+
+        for (int j = 0; j < 20; j++) {
+            System.out.println((j + 1) + " table's elemnt is: " + table[j]);
+        }
+    }
 }
