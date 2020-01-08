53bb388b824b67be0b79ed975befdcba507e7a9e
Dorota Miler <dorota.miler@outlook.com>
2020-01-08 18:54:44 +0100

    For. java example

--- src/main/java/_9_loops/For.java
+++ src/main/java/_9_loops/For.java
@@ -1,4 +1,10 @@
 package _9_loops;
 
 public class For {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("This is a for loop, line: " + i);
+        }
+        System.out.println("The end of a for loop");
+    }
 }
