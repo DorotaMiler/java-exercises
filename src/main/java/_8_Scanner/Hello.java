4f67b8f2d862769a1cece000338cbe85c1e150bd
Dorota Miler <dorota.miler@outlook.com>
2020-01-08 23:18:30 +0100

    NumberAndString.java displaying advantage of utilizing scan.nextLine() for correct programms run

--- src/main/java/_8_Scanner/Hello.java
+++ src/main/java/_8_Scanner/Hello.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in); // system allows readout from the indicated source - here System.in therefore from console
         System.out.println("What is your name?");
         String name = scan.nextLine();
         System.out.println("Welcome " + name);
---
+++ src/main/java/_8_Scanner/NumberAndString.java
@@ -0,0 +1,16 @@
+package _8_Scanner;
+
+import java.util.Scanner;
+
+public class NumberAndString {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please write a number: ");
+        int number = scan.nextInt();
+        scan.nextLine();
+        System.out.println("Please write a word: ");
+        String word = scan.nextLine();
+
+        System.out.println(word + ", " + number);
+    }
+}
