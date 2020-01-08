6dbaebaea2358d5c49f91fde98130dfde3041f5a
Dorota Miler <dorota.miler@outlook.com>
2020-01-08 23:03:21 +0100

    Hello.java with simple System.in programm

--- src/main/java/_8_Scanner/Hello.java
+++ src/main/java/_8_Scanner/Hello.java
@@ -1,4 +1,12 @@
 package _8_Scanner;
 
+import java.util.Scanner;
+
 public class Hello {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = scan.nextLine();
+        System.out.println("Welcome " + name);
+    }
 }
