82b76720c62b621b1affa081c1513d28a300f43d
Dorota Miler <dorota.miler@outlook.com>
2020-01-09 22:45:23 +0100

    SetAPoint.java arrangement

--- src/main/java/_b_object_programming/Coordinates/SetAPoint.java
+++ src/main/java/_b_object_programming/Coordinates/SetAPoint.java
@@ -2,7 +2,9 @@ package _b_object_programming.Coordinates;
 
 public class SetAPoint {
     public static void main(String[] args) {
-        SetAPoint point = new SetAPoint();
-        point.
+        Point point = new Point();
+        point.coordinatesX = 10;
+        point.coordinatesY = 20;
+        System.out.println("Point's coordinates are: (" + point.coordinatesX + ", " + point.coordinatesY + ")");
     }
 }
