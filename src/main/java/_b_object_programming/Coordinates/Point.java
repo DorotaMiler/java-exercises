5aab350066caff8e143991741d25d6994f0ec310
Dorota Miler <dorota.miler@outlook.com>
2020-01-09 22:50:38 +0100

    Point.java methods addition

--- src/main/java/_b_object_programming/Coordinates/Point.java
+++ src/main/java/_b_object_programming/Coordinates/Point.java
@@ -3,4 +3,20 @@ package _b_object_programming.Coordinates;
 public class Point {
     int coordinatesX;
     int coordinatesY;
+
+    void setX(int x) {
+        coordinatesX = x;
+    }
+
+    void setY(int y) {
+        coordinatesY = y;
+    }
+
+    int returnX() {
+        return coordinatesX;
+    }
+
+    int returnY() {
+        return coordinatesY;
+    }
 }
