26c6ae0203029143ae285aa9b36519ab6945dd9a
Dorota Miler <dorota.miler@outlook.com>
2020-01-09 22:13:13 +0100

    change from Table.java name to Array.java

--- src/main/java/_10_tables/Array.java
+++ src/main/java/_10_tables/Array.java
@@ -1,6 +1,6 @@
 package _10_tables;
 
-public class Table {
+public class Array {
     public static void main(String[] args) {
         int[] table = new int[20];
         for (int i = 0; i < 20; i++) {
