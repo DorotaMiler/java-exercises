c08ca6f433cac6bce85b8fd70011d4cd9940cfb4
Dorota Miler <dorota.miler@outlook.com>
2020-01-13 22:54:05 +0100

    Main.java with 2 static methods differing by parameters, name stays the same

--- src/main/java/_d_overloading/Main.java
+++ src/main/java/_d_overloading/Main.java
@@ -1,4 +1,17 @@
 package _d_overloading;
 
 public class Main {
+    public static void main(String[] args) {
+        displayName();
+        displayName("Dorota");
+    }
+
+    private static void displayName() {
+        System.out.println("Monika");
+    }
+
+    private static void displayName(String name){
+        System.out.println(name);
+    }
+
 }
