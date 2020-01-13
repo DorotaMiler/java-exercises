d9dd34e190ac6ab7ccfdd02864809ce6ede25940
Dorota Miler <dorota.miler@outlook.com>
2020-01-13 23:13:29 +0100

    object porgraming package -> Main modificaion - addition of an addMultiple()

--- src/main/java/_b_object_programming/Coordinates/_1_methods_arguments/Main.java
+++ src/main/java/_b_object_programming/Coordinates/_1_methods_arguments/Main.java
@@ -11,7 +11,8 @@ public class Main {
         int a = 1;
         int b = 2;
         int c = 3;
-        int result = add(a, b, c);
+        double d = 4.0;
+        double result = addMultiple(a, b, c, d);
         System.out.println("run() -> result = " + result);
         System.out.println("run() -> a = " + a);
         System.out.println("run() -> a = " + b);
@@ -21,5 +22,13 @@ public class Main {
     private int add(int a, int b, int c) {
         a = 10;
         return a + b + c;
+    }
+
+    private double addMultiple(double... numbers) {
+        double sum = 0;
+        for (double number : numbers) {
+            sum += sum + number;
+        }
+        return sum;
     }
 }
