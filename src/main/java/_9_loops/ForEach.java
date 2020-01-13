66ad0244e768d787d8dca1d40cc2077d2ae2ff3a
Dorota Miler <dorota.miler@outlook.com>
2020-01-13 19:02:26 +0100

    ForEach.java additional example addition

--- src/main/java/_9_loops/ForEach.java
+++ src/main/java/_9_loops/ForEach.java
@@ -4,14 +4,22 @@ public class ForEach {
     public static void main(String[] args) {
 
         // filling in table with numbers from1 to 10
-        int [] table = new int[10];
-        for (int i = 0; i <10; i++){
+        int[] table = new int[10];
+        for (int i = 0; i < 10; i++) {
             table[i] = i + 1;
         }
 
         // display of the created table
-        for (int x : table){
-            System.out.println(x + " element of the table = "+ x);
+        for (int x : table) {
+            System.out.println(x + " element of the table = " + x);
         }
+        // second for each loop
+
+        String[] names = {"Dorota", "Adam", "Kuba", "Filip", "Dagmara"};
+        System.out.println("\n");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Array's " + i + " index name is: " + names[i]);
+        }
+
     }
 }
