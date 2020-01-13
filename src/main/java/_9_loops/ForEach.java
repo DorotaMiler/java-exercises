e83425f5d78d991a7c73e0d88d065c2ddb0e670d
Dorota Miler <dorota.miler@outlook.com>
2020-01-13 19:14:44 +0100

    List loop example + comments addition

--- src/main/java/_9_loops/ForEach.java
+++ src/main/java/_9_loops/ForEach.java
@@ -1,5 +1,8 @@
 package _9_loops;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ForEach {
     public static void main(String[] args) {
 
@@ -15,17 +18,24 @@ public class ForEach {
         }
         // second for loop
         String[] names = {"Dorota", "Adam", "Kuba", "Filip", "Dagmara"};
-        System.out.println("\n");
+        System.out.println("\nFor loop example: ");
         for (int i = 0; i < names.length; i++) {
             System.out.println("Array's " + i + " index name is: " + names[i]);
         }
 
         // for each loop
-        System.out.println("\n");
+        System.out.println("\nFor each loop example: ");
         for (String name : names) {
             System.out.println(name);
         }
 
+List<String> strings = Arrays.asList(names);
+        System.out.println("\nLoop based on List git add ." +
+                "git commit -m ''example: ");
+        for (String string : strings) {
+            System.out.println(string);
+
+        }
 
     }
 
