3bdb05823948b647384ac6e2061fbb6cec87294b
Dorota Miler <dorota.miler@outlook.com>
2020-01-13 19:08:59 +0100

    ForEach.java for each straight example reflecting above for example

--- src/main/java/_9_loops/ForEach.java
+++ src/main/java/_9_loops/ForEach.java
@@ -13,13 +13,21 @@ public class ForEach {
         for (int x : table) {
             System.out.println(x + " element of the table = " + x);
         }
-        // second for each loop
-
+        // second for loop
         String[] names = {"Dorota", "Adam", "Kuba", "Filip", "Dagmara"};
         System.out.println("\n");
         for (int i = 0; i < names.length; i++) {
             System.out.println("Array's " + i + " index name is: " + names[i]);
         }
 
+        // for each loop
+        System.out.println("\n");
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+
     }
+
 }
+
