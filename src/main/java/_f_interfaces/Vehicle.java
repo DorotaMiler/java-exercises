e13e9a59464717f7440d1e24f9e9879c3e96bf56
Dorota Miler <dorota.miler@outlook.com>
2020-01-14 19:14:20 +0100

    comment to interface Vehicle.java within interfaces package

--- src/main/java/_f_interfaces/Vehicle.java
+++ src/main/java/_f_interfaces/Vehicle.java
@@ -1,5 +1,12 @@
 package _f_interfaces;
 
+/**
+ * all METHODS are by default public and abstract
+ * (nothing happens if we forget to state that)
+ * all FIELDS must be declared as public, final and static without need
+ * to indicate them as final or static
+ */
+
 public class Vehicle {
     public void move(int velocity);
 
