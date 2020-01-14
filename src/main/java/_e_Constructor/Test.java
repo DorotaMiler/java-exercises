be3381a65148db1298e6fb5d69ac564d17c10661
Dorota Miler <dorota.miler@outlook.com>
2020-01-14 18:16:07 +0100

    Test.java implementing constructor based on previous object

--- src/main/java/_e_Constructor/Test.java
+++ src/main/java/_e_Constructor/Test.java
@@ -6,5 +6,10 @@ public class Test {
         System.out.println("Coordinates of object 'point' are: ");
         System.out.println("x: " + point.x);
         System.out.println("y: " + point.y);
+
+        Point point1 = new Point(point);
+        System.out.println("\nCoordinates of object 'point1' are: ");
+        System.out.println("x: " + point1.x);
+        System.out.println("y: " + point1.y);
     }
 }
