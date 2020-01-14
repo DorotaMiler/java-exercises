84dd63890bd7732f5a36aa976b194f493ce429af
Dorota Miler <dorota.miler@outlook.com>
2020-01-14 18:09:16 +0100

    coordinates display in Test.java + formatting

--- src/main/java/_e_Constructor/Point.java
+++ src/main/java/_e_Constructor/Point.java
@@ -4,7 +4,7 @@ public class Point {
     int x;
     int y;
 
-    public Point(){
+    public Point() {
     }
 
     public Point(int a, int b) {
@@ -12,7 +12,7 @@ public class Point {
         y = b;
     }
 
-    public Point(Point point){
+    public Point(Point point) {
         x = point.x;
         y = point.y;
     }
--- src/main/java/_e_Constructor/Test.java
+++ src/main/java/_e_Constructor/Test.java
@@ -1,4 +1,10 @@
 package _e_Constructor;
 
 public class Test {
+    public static void main(String[] args) {
+        Point point = new Point(10, 20);
+        System.out.println("Coordinates of object 'point' are: ");
+        System.out.println("x: " + point.x);
+        System.out.println("y: " + point.y);
+    }
 }
