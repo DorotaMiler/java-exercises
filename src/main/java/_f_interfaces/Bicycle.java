b2a1f6620031904e8f0a50e40f8a5dd34a6e931e
Dorota Miler <dorota.miler@outlook.com>
2020-01-14 19:07:33 +0100

    Car.java & Bicycle.java implement Vehicle interface + 1 own method each

--- src/main/java/_f_interfaces/Bicycle.java
+++ src/main/java/_f_interfaces/Bicycle.java
@@ -1,4 +1,15 @@
 package _f_interfaces;
 
-public class Bicycle {
+public class Bicycle implements Vehicle {
+
+    @Override
+    public void move(int velocity) {
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    public void jump() {
+    }
 }
--- src/main/java/_f_interfaces/Car.java
+++ src/main/java/_f_interfaces/Car.java
@@ -1,4 +1,15 @@
 package _f_interfaces;
 
-public class Car {
+public class Car implements Vehicle {
+
+    @Override
+    public void move(int velocity) {
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    public void drift() {
+    }
 }
