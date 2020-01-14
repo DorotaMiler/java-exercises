34db7f882b0dff24694539deef0784f8fb3fdcf8
Dorota Miler <dorota.miler@outlook.com>
2020-01-14 19:19:10 +0100

    Vehicle.java interface -> comments

--- src/main/java/_f_interfaces/Vehicle.java
+++ src/main/java/_f_interfaces/Vehicle.java
@@ -1,10 +1,13 @@
 package _f_interfaces;
 
 /**
- * all METHODS are by default public and abstract
+ * -> all METHODS are by default public and abstract (cannot be static)
  * (nothing happens if we forget to state that)
- * all FIELDS must be declared as public, final and static without need
+ * -> all FIELDS must be declared as public, final and static without need
  * to indicate them as final or static
+ * -> interface can extend only interfaces and even a couple of them
+ * -> you can implement many interfaces (in contrast to a ban of multiple
+ * inheritance)
  */
 
 public class Vehicle {
