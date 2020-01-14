5a9e3c955115f8ddcdbdb6eaed0059b81b1ff4c6
Dorota Miler <dorota.miler@outlook.com>
2020-01-14 18:40:40 +0100

    Test.java within constructors package - addition of 2nd point

--- src/main/java/_e_Constructor/Test.java
+++ src/main/java/_e_Constructor/Test.java
@@ -11,5 +11,18 @@ public class Test {
         System.out.println("\nCoordinates of object 'point1' are: ");
         System.out.println("x: " + point1.x);
         System.out.println("y: " + point1.y);
+
+        Point point2 = point;
+
+        point2.x = 34;
+        point2.y = 43;
+
+        System.out.println("\nCoordinates of object 'point2' are: ");
+        System.out.println("x: " + point2.x);
+        System.out.println("y: " + point2.y);
+
+        System.out.println("\nAGAIN (for comparison) Coordinates of object 'point' are: ");
+        System.out.println("x: " + point.x);
+        System.out.println("y: " + point.y);
     }
 }
