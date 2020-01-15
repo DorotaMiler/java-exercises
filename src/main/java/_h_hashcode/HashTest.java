ce00f5f85ded5b51d3421aaf755008b402362f71
Dorota Miler <dorota.miler@outlook.com>
2020-01-15 23:46:45 +0100

    HashTest.java to compare resulats from equals() and hascode() on both objects

--- src/main/java/_h_hashcode/HashTest.java
+++ src/main/java/_h_hashcode/HashTest.java
@@ -7,6 +7,7 @@ public class HashTest {
         Product product2 = new Product("Czekolada",3.99);
         System.out.println("Equals() result: "+product1.equals(product2));
         // below default hashcode() method for calculation utilizez object's address in memory
+        // this value is utilized in toString() in hexadecimal version
         System.out.println("Product1 hashcode: "+product1.hashCode());
         System.out.println("Product2 hasgcode: "+product2.hashCode());
     }
