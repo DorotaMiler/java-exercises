56e0ea2940c295e74f0b49385a539e288f92594d
Dorota Miler <dorota.miler@outlook.com>
2020-01-16 18:56:05 +0100

    HashTest.java modification -> Path class utilized to cteate object of an accual path  and hashcoding it to int hashCode variable

--- src/main/java/_f_interfaces_plus_super/Vehicle.java
+++ src/main/java/_f_interfaces_plus_super/Vehicle.java
@@ -20,5 +20,4 @@ public interface Vehicle {
 
     void measureVelocity(int velocity);
 
-    hashCode
 }
--- src/main/java/_h_hashcode/HashTest.java
+++ src/main/java/_h_hashcode/HashTest.java
@@ -7,15 +7,18 @@ import java.nio.file.Paths;
 public class HashTest {
 
     public static void main(String[] args) throws IOException {
-        Product product1 = new Product("Czekolada",3.99);
-        Product product2 = new Product("Czekolada",3.99);
-        System.out.println("Equals() result: "+product1.equals(product2));
+        Product product1 = new Product("Czekolada", 3.99);
+        Product product2 = new Product("Czekolada", 3.99);
+        System.out.println("Equals() result: " + product1.equals(product2));
         // below default hashcode() method for calculation utilizez object's address in memory
         // this value is utilized in toString() in hexadecimal version
-        System.out.println("Product1 hashcode: "+product1.hashCode());
-        System.out.println("Product2 hasgcode: "+product2.hashCode());
+        System.out.println("Product1 hashcode: " + product1.hashCode());
+        System.out.println("Product2 hasgcode: " + product2.hashCode());
 
         // object of Path created
-        Path path = Paths.get(./home/dorotamiler/Desktop/Dorota Miler_CV.pdf)
+        Path path = Paths.get("home/dorotamiler/Desktop/Dorota Miler_CV.pdf");
+
+        int hashCode = path.hashCode();
+        System.out.println("Hash Code: " + hashCode);
     }
 }
