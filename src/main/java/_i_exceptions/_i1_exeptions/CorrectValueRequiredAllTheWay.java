99363c24d82f1c41c33581a3e452b3ddc0b09e7e
Dorota Miler <dorota.miler@outlook.com>
2020-01-17 23:08:44 +0100

    CorrectValueRequiredAllTheWay.java souts accomodation

--- src/main/java/_i_exceptions/_i1_exeptions/CorrectValueRequiredAllTheWay.java
+++ src/main/java/_i_exceptions/_i1_exeptions/CorrectValueRequiredAllTheWay.java
@@ -10,12 +10,13 @@ public class CorrectValueRequiredAllTheWay {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int index = -2;
         boolean x = false;
+        System.out.println("Please enter the index of which array's number you want to display: ");
 
         while (!x) {
             try {
                 index = Integer.parseInt(reader.readLine());
             } catch (NumberFormatException n) {
-                System.out.println("Not correct format of data :(" + "\nReenter table's index you want to invoke: ");
+                System.out.println("Not correct format of data :(" + "\nReenter array's index you want to invoke: ");
             } catch (IOException e) {
                 System.out.println("Error! - data read-out issue");
             }
@@ -23,7 +24,7 @@ public class CorrectValueRequiredAllTheWay {
         }
 
         try {
-            System.out.println(table[index - 1]);
+            System.out.println("SUCCESS:" + "\n Array's number correspondent with given index is: " + table[index - 1]);
         } catch (IndexOutOfBoundsException e) {
             System.out.println("Given index not accurate (out of bound). Table's length is: " + table.length);
         }
