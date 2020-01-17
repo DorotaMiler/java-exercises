b33fd416c2b9bccdb5a4dfb5763228acdc0a99d5
Dorota Miler <dorota.miler@outlook.com>
2020-01-17 17:40:44 +0100

    Main.java within exceptions package -> comments addition

--- src/main/java/_i_exceptions/Main.java
+++ src/main/java/_i_exceptions/Main.java
@@ -11,17 +11,18 @@ public class Main {
         // example with scanner
         int[] table = {1, 2, 3, 4, 5};
         Scanner scan = new Scanner(System.in);
-        int index = -1;
 
         System.out.println("\nPlease advise of a chosen index, of which number you want to see: ");
-        index = scan.nextInt();
+        int index = scan.nextInt();
 
+//        1a) solution:
 //        if (index >= 0 && index < table.length) {
 //            System.out.println("Due to chosen index, array's number is: " + table[index]);
 //        } else {
 //            System.out.println("The given index is incorrect, please verify (array's length is: " + table.length + " elements.");
 //        }
 
+        // 1b? solution
         try {
             System.out.println("Due to chosen index, array's number is: " + table[index]);
         } catch (ArrayIndexOutOfBoundsException e) {
