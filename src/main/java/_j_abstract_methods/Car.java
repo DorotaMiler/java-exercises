f5e236f372e3c38f667344b6fc63a81cdae0f4e8
Dorota Miler <dorota.miler@outlook.com>
2020-01-17 23:43:32 +0100

    Car.java from abstract methods package enriched with display of how to avoid duplication in extending class

--- src/main/java/_j_abstract_methods/Car.java
+++ src/main/java/_j_abstract_methods/Car.java
@@ -1,4 +1,11 @@
 package _j_abstract_methods;
 
-public class Car {
+import com.sun.xml.internal.ws.api.pipe.Engine;
+
+public class Car extends Vehicle{
+    private static final int NUMBER_OF_WHEELS = 4;
+
+    public Car(Engine engine) {
+        super(engine, NUMBER_OF_WHEELS);
+    }
 }
