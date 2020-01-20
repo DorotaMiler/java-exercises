363b1afd3caa7f62131cf3539b1a09534d4200f3
Dorota Miler <dorota.miler@outlook.com>
2020-01-20 21:02:00 +0100

    FileManager.java - types of messages

--- src/main/java/_i_exceptions/_i3_exeptions_must_be_handled/FileManager.java
+++ src/main/java/_i_exceptions/_i3_exeptions_must_be_handled/FileManager.java
@@ -5,11 +5,21 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class FileManager {
-    File file = new File("test.txt");
+    public static void main(String[] args) {
 
-    try{
-        Scanner scan = new Scanner(file);
-    }catch (FileNotFoundException e){
-        e.printStackTrace();
+        File file = new File("test.txt");
+
+        try {
+            Scanner scan = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            // personalized message
+            System.out.println("There is no such file");
+            // test.txt (No such file or directory)
+            System.out.println(e.getMessage());
+            // test.txt (No such file or directory)
+            System.out.println(e.getLocalizedMessage());
+            // pile of branch control structure (run for review :))
+            e.printStackTrace();
+        }
     }
 }
