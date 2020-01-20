6b3780aafdecbb0b41f7b8c5e1a406b12e276f1b
Dorota Miler <dorota.miler@outlook.com>
2020-01-20 18:34:39 +0100

    exceptions package small corrections + rewrote task for exercise

--- src/main/java/_i_exceptions/HourToSeconds.java
+++ src/main/java/_i_exceptions/HourToSeconds.java
@@ -3,7 +3,7 @@ package _i_exceptions;
 public class HourToSeconds {
     public int getNumberOfSeconds(int hour) {
         if (hour < 0) {
-            throw new IllegalArgumentException("Hour must be >= ;0: " + hour);
+            throw new IllegalArgumentException("Hour must be >= 0: " + hour);
         } else {
             return hour * 60 * 60;
         }
--- src/main/java/_i_exceptions/_i1_exeptions/CorrectValueRequiredAllTheWay.java
+++ src/main/java/_i_exceptions/_i1_exeptions/CorrectValueRequiredAllTheWay.java
@@ -8,40 +8,24 @@ public class CorrectValueRequiredAllTheWay {
     public static void main(String[] args) {
         int[] table = {1, 2, 3, 4, 5, 6};
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int index = 1;
+        int index = -2;
         boolean x = false;
+        System.out.println("Please enter the index of which array's number you want to display: ");
 
         while (!x) {
             try {
                 index = Integer.parseInt(reader.readLine());
             } catch (NumberFormatException n) {
-                System.out.println("Not correct format of data :(\" + \"\\nReenter array's index you want to invoke: ");
+                System.out.println("Not correct format of data :(" + "\nReenter correct array's index you want to invoke: ");
             } catch (IOException e) {
                 System.out.println("Error! - data read-out issue");
             }
-
-
-
-//        int[] table = {1, 2, 3, 4, 5};
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//        int index = -2;
-//        boolean x = false;
-//        System.out.println("Please enter the index of which array's number you want to display: ");
-//
-//        while (!x) {
-//            try {
-//                index = Integer.parseInt(reader.readLine());
-//            } catch (NumberFormatException n) {
-//                System.out.println("Not correct format of data :(" + "\nReenter array's index you want to invoke: ");
-//            } catch (IOException e) {
-//                System.out.println("Error! - data read-out issue");
-//            }
-//            x = index == -2 ? false : true;
+            x = index == -2 ? false : true;
         }
 
         try {
-            System.out.println("SUCCESS:" + "\n Array's number correspondent with given index is: " + table[index - 1]);
-        } catch (IndexOutOfBoundsException e) {
+            System.out.println("SUCCESS:" + "\n Array's number correspondent with given index is:" + table[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Given index not accurate (out of bound). Table's length is: " + table.length);
         }
     }
