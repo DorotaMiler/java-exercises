1c10dbf92cbced7d4550ac4ff9163bf0add5b392
DorotaMiler <dorota.miler@outlook.com>
2020-01-27 23:51:25 +0100

    Engine.java -> equals), hashcode() & toString() override

--- src/main/java/_j_abstract_methods/Engine.java
+++ src/main/java/_j_abstract_methods/Engine.java
@@ -1,12 +1,12 @@
 package _j_abstract_methods;
 
+import java.util.Objects;
+
 public class Engine {
 
     private Integer power;
     private Integer capacity;
-
     private String name;
-
     private String name1 = "Inline";
     private String name2 = "VR";
     private String name3 = "Boxer";
@@ -43,5 +43,35 @@ public class Engine {
 
     public void setPower(Integer power) {
         this.power = power;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Engine)) return false;
+        Engine engine = (Engine) o;
+        return Objects.equals(getPower(), engine.getPower()) &&
+                Objects.equals(getCapacity(), engine.getCapacity()) &&
+                Objects.equals(name, engine.name) &&
+                Objects.equals(getName1(), engine.getName1()) &&
+                Objects.equals(getName2(), engine.getName2()) &&
+                Objects.equals(getName3(), engine.getName3());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPower(), getCapacity(), name, getName1(), getName2(), getName3());
+    }
+
+    @Override
+    public String toString() {
+        return "Engine{" +
+                "power=" + power +
+                ", capacity=" + capacity +
+                ", name='" + name + '\'' +
+                ", name1='" + name1 + '\'' +
+                ", name2='" + name2 + '\'' +
+                ", name3='" + name3 + '\'' +
+                '}';
     }
 }
