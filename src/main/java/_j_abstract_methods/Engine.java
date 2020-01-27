ab8c3c5258dc228bb1e946a690cab6af09f9dd34
DorotaMiler <dorota.miler@outlook.com>
2020-01-27 23:27:54 +0100

    Engine.java - fileds addition + setters and getters

--- src/main/java/_j_abstract_methods/Engine.java
+++ src/main/java/_j_abstract_methods/Engine.java
@@ -2,6 +2,9 @@ package _j_abstract_methods;
 
 public class Engine {
 
+    private Integer power;
+    private Integer capacity;
+
     private String inline = "Inline";
     private String vr = "VR";
     private String boxer = "Boxer";
@@ -16,5 +19,21 @@ public class Engine {
 
     public String getVr() {
         return vr;
+    }
+
+    public Integer getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(Integer capacity) {
+        this.capacity = capacity;
+    }
+
+    public Integer getPower() {
+        return power;
+    }
+
+    public void setPower(Integer power) {
+        this.power = power;
     }
 }
