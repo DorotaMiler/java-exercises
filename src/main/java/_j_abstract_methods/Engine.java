a792e168a151d5587cd273a9797a3946d73055c3
DorotaMiler <dorota.miler@outlook.com>
2020-01-27 20:51:14 +0100

    abstract methods practice - addition of Engines variables

---
+++ src/main/java/_j_abstract_methods/Engine.java
@@ -0,0 +1,20 @@
+package _j_abstract_methods;
+
+public class Engine {
+
+    private String inline = "Inline";
+    private String vr = "VR";
+    private String boxer = "Boxer";
+
+    public String getBoxer() {
+        return boxer;
+    }
+
+    public String getInline() {
+        return inline;
+    }
+
+    public String getVr() {
+        return vr;
+    }
+}
