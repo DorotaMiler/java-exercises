53cd40418ec88d6cd20c713edb13a4e38e120599
DorotaMiler <dorota.miler@outlook.com>
2020-01-27 23:54:32 +0100

    Vehicle.class -> new field: Date of production date + getters and setter

--- src/main/java/_j_abstract_methods/Vehicle.java
+++ src/main/java/_j_abstract_methods/Vehicle.java
@@ -1,9 +1,12 @@
 package _j_abstract_methods;
 
+import java.util.Date;
+
 public abstract class Vehicle {
 
     private int numberOfWheels;
     private Engine engine;
+    private Date producionDate;
 
     public Vehicle(Engine engine, int numberOfWheels) {
         this.engine = engine;
@@ -16,5 +19,17 @@ public abstract class Vehicle {
 
     public Engine getEngine() {
         return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Date getProducionDate() {
+        return producionDate;
+    }
+
+    public void setProducionDate(Date producionDate) {
+        this.producionDate = producionDate;
     }
 }
