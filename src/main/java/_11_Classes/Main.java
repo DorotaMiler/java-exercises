a6a2c034d8b31d46fa4d7e70b0c779c8c1fd80d7
DorotaMiler <dorota.miler@outlook.com>
2020-02-02 13:20:49 +0100

    basic classes overview - Object.java as first

--- src/main/java/_11_Classes/Main.java
+++ src/main/java/_11_Classes/Main.java
@@ -1,4 +1,10 @@
 package _11_Classes;
 
 public class Main {
+
+    public static void main(String[] args) {
+        ObjectsClass objectClass1 = new ObjectsClass();
+        ObjectsClass objectsClass2 = new ObjectsClass();
+        System.out.println(objectClass.compareObjects(objectClass1,objectsClass2));
+    }
 }
--- src/main/java/_11_Classes/ObjectsClass.java
+++ src/main/java/_11_Classes/ObjectsClass.java
@@ -1,7 +1,9 @@
 package _11_Classes;
 
 public class ObjectsClass {
-    private boolean compareObjects(Object o1, Object o2){
+    public boolean compareObjects(Object o1, Object o2){
         return o1.equals(o2);
     }
+
+
 }
