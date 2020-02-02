6a74fda8bd25c0ed5315d7610532d2d1b095b21e
DorotaMiler <dorota.miler@outlook.com>
2020-02-02 13:53:17 +0100

    easy calculator in CalculatorEx.java

---
+++ src/main/java/_4_math_and_logic_operators/CalculatorEx.java
@@ -0,0 +1,16 @@
+package _4_math_and_logic_operators;
+
+public class CalculatorEx {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 3;
+        double result = a / b;
+        System.out.println(result);
+
+        double c = (double) a;
+        double d = (double) b;
+        double result1 = c / d;
+        System.out.println(result1);
+    }
+}
