861536d26d44675810d45397249c10ed706cf6d2
DorotaMiler <dorota.miler@outlook.com>
2020-02-03 23:36:05 +0100

    data types ex with assignment operators

--- src/main/java/_5_math_functions_BigInteger_BigDecimal/NormalNumbers.java
+++ src/main/java/_5_math_functions_BigInteger_BigDecimal/NormalNumbers.java
@@ -7,5 +7,12 @@ public class NormalNumbers {
         double c = 0.33;
         System.out.println("Calculation of a, b & c = " + (a / b * c)); // not precise result due
         // to a fact that doubles are only rounded numbers
+
+        Integer d = 2 * 30;
+        Double e = d + c;
+        System.out.println(e);
+        e += 10;
+        System.out.println(d);
+        System.out.println(e);
     }
 }
