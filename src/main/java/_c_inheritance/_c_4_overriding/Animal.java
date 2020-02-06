c5172b3115d6a1c5c3d094b416dfacd7f14b7f12
DorotaMiler <dorota.miler@outlook.com>
2020-02-06 18:45:27 +0100

    basic classes - Animal & Elephant; Animal with fields and adequate getters, setters

---
+++ src/main/java/_c_inheritance/_c_4_overriding/Animal.java
@@ -0,0 +1,31 @@
+package _c_inheritance._c_4_overriding;
+
+public class Animal {
+    private String name;
+    private Integer age;
+    private double weight;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+    
+    public double getWeight() {
+        return weight;
+    }
+}
---
+++ src/main/java/_c_inheritance/_c_4_overriding/Elephant.java
@@ -0,0 +1,4 @@
+package _c_inheritance._c_4_overriding;
+
+public class Elephant {
+}
