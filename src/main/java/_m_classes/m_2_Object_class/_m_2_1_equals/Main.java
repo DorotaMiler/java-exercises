07aa4c3e445c2912ff776e21af33327dacfa4fa4
DorotaMiler <dorota.miler@outlook.com>
2020-02-10 21:47:52 +0100

    example of Strings == & equals() + hashcode printout

--- src/main/java/_m_classes/m_2_Object_class/_m_2_1_equals/Main.java
+++ src/main/java/_m_classes/m_2_Object_class/_m_2_1_equals/Main.java
@@ -4,8 +4,14 @@ public class Main {
     public static void main(String[] args) {
         String s1 = new String("abc");
         String s2 = new String("abc");
-        System.out.println("Reference comparison -> ");
-        System.out.println(s1 == s2);
-        System.out.println("s1.equals(s2) -> "+s1.equals(s2));  // true
+        System.out.println("Reference comparison -> " + (s1 == s2));
+        System.out.println("s1.equals(s2) -> " + s1.equals(s2)); // true
+
+        Test test = new Test();
+        test.referenceComarison();
+        test.equalsComparison();
+        System.out.println(test.a.hashCode());
+        System.out.println(test.b.hashCode());
+
     }
 }
--- src/main/java/_m_classes/m_2_Object_class/_m_2_1_equals/Test.java
+++ src/main/java/_m_classes/m_2_Object_class/_m_2_1_equals/Test.java
@@ -1,4 +1,23 @@
 package _m_classes.m_2_Object_class._m_2_1_equals;
 
 public class Test {
+
+    String a = "string1";
+    String b = "string2";
+
+    public void referenceComarison() {
+        if (a == b) {
+            System.out.println("a == b");
+        } else {
+            System.out.println("a !== b");
+        }
+    }
+
+    public void equalsComparison() {
+        if (a.equals(b)) {
+            System.out.println("a equals() b");
+        } else {
+            System.out.println("a doesn't equal() b");
+        }
+    }
 }
