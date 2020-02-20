0dd478df6ffc66b7ed87f34073aaff72cbe035d9
DorotaMiler <dorota.miler@outlook.com>
2020-02-20 22:31:07 +0100

    StringSeparators -> working on colonsInString() -> initialization of String to utilize concat & trim() in the end which gets rid of last blank space at the end of the sentence

--- src/main/java/_j_classes/_j_3_String/StringSeparators.java
+++ src/main/java/_j_classes/_j_3_String/StringSeparators.java
@@ -1,28 +1,24 @@
 package _j_classes._j_3_String;
 
-import java.util.Scanner;
-
 public class StringSeparators {
 
     public static void colonsInString() {
         String string = "string:separated:by:colons";
         String[] stringArray = string.split(":");
         System.out.println("Strings array's length: " + stringArray.length);
+        String newString = "";
         for (int i = 0; i < stringArray.length; i++) {
-            String newString = stringArray[i] + " ";
-            if (i == stringArray.charAt(stringArray.length - 1)) {
-                System.out.println(newString.trim());
-            }
+            newString = newString.concat(stringArray[i] + " ");
         }
+        System.out.println(newString.trim());
     }
+}
 
 //    dotsInString() {
 //
 //    }
 
-    public String scannerIntro(String inputLine) {
-        Scanner scan = new Scanner(System.in);
-        inputLine = scan.nextLine();
-    }
-
-}
+//    public String scannerIntro(String inputLine) {
+//        Scanner scan = new Scanner(System.in);
+//        inputLine = scan.nextLine();
+//    }
