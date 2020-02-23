e93c4423ce3d5b6faa4f216fb1302c7834f6e40c
DorotaMiler <dorota.miler@outlook.com>
2020-02-23 12:27:02 +0100

    comment addition

--- src/main/java/_j_classes/_j_3_String/StringConcat.java
+++ src/main/java/_j_classes/_j_3_String/StringConcat.java
@@ -5,6 +5,9 @@ import java.util.StringJoiner;
 public class StringConcat {
 
     public static void stringsJoining() {
+        // note: String immutable; join() utilizez StringJoiner which uses
+        // StringBuilder therefore processing profitability maintained
+
         StringJoiner joiner = new StringJoiner(" ", "{", "}");
         String result = joiner.add("Dorota").add("is").add("ill").add("and").add("stays").add("home").toString();
         System.out.println(result);
