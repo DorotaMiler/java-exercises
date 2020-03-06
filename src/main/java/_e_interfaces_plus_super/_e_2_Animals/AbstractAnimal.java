b08294a112a9b1fcac269b9b3cbe35d91e2ae4dd
DorotaMiler <dorota.miler@outlook.com>
2020-03-06 21:37:46 +0100

    AbstractAnimal creation + own Exception for validation of regular abstrac classes method

---
+++ src/main/java/_e_interfaces_plus_super/_e_2_Animals/AbstractAnimal.java
@@ -0,0 +1,15 @@
+package _e_interfaces_plus_super._e_2_Animals;
+
+public abstract class AbstractAnimal {
+
+    abstract void allAnimlsLive();
+
+    public void takeCareOfAnimals (int timesAWeek){
+        while(timesAWeek=<7){
+            try {
+                System.out.println("Animals need to be taken care of " + timesAWeek + " times a week.");
+            } catch (Exception)
+        }
+
+
+}
---
+++ src/main/java/_e_interfaces_plus_super/_e_2_Animals/AmountOfDaysOutOfBondException.java
@@ -0,0 +1,4 @@
+package _e_interfaces_plus_super._e_2_Animals;
+
+public class AmountOfDaysOutOfBondException {
+}
