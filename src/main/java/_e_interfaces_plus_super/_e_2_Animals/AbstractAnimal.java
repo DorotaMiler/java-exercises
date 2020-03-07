7a300adfcda9d2204acd3660a40718e832343ce3
DorotaMiler <dorota.miler@outlook.com>
2020-03-07 23:35:21 +0100

    Exception name change -> to DaysOverLimitException + shortening of parameters in this Exception constructor

--- src/main/java/_e_interfaces_plus_super/_e_2_Animals/AbstractAnimal.java
+++ src/main/java/_e_interfaces_plus_super/_e_2_Animals/AbstractAnimal.java
@@ -8,11 +8,9 @@ public abstract class AbstractAnimal {
         while (timesAWeek =<7){
             try {
                 System.out.println("Animals need to be taken care of " + timesAWeek + " times a week.");
-            } catch (Exception e){
-                throw AmountOfDaysOutOfBondException
+            } catch (Exception e) {
+                throw new DaysOverLimitException("Incorrect amount of days: " + timesAWeek);
             }
         }
-
-
     }
 }
---
+++ src/main/java/_e_interfaces_plus_super/_e_2_Animals/DaysOverLimitException.java
@@ -0,0 +1,9 @@
+package _e_interfaces_plus_super._e_2_Animals;
+
+public class DaysOverLimitException extends Exception {
+
+    public DaysOverLimitException(String message) {
+        super(message);
+    }
+
+}
