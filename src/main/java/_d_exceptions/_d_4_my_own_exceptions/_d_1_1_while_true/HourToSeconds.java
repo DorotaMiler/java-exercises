106017bf540f9defa637216a0ee0acd424c6968f
DorotaMiler <dorota.miler@outlook.com>
2020-03-09 23:20:48 +0100

    correction of sout info + convertHourToSeconds() name change and adequate changes within methods body for better execution

--- src/main/java/_d_exceptions/_d_4_my_own_exceptions/_d_1_1_while_true/HourToSeconds.java
+++ src/main/java/_d_exceptions/_d_4_my_own_exceptions/_d_1_1_while_true/HourToSeconds.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class HourToSeconds {
 
-    public int getNumberOfSeconds(int hour) throws IllegalArgumentException {
+    public Integer getNumberOfSeconds(Integer hour) throws IllegalArgumentException {
         if (hour < 0) {
             throw new IllegalArgumentException("Hour must be integer >= 0: (you wrote) " + hour + " -> please revise");
         } else {
@@ -12,25 +12,26 @@ public class HourToSeconds {
         }
     }
 
-    public void scanHour() throws IllegalArgumentException{
-        int hours = -4;
-        int numberOfSeconds = 0;
+    public void convertHourToSeconds() throws IllegalArgumentException {
+
+        Integer numberOfSeconds = 0;
         boolean isNumberCorrect = true;
         HourToSeconds hourToSeconds = new HourToSeconds();
-        System.out.println("Please advise of an amount of hours you would like to convert into correspondent amount of seconds: ");
+        System.out.println("Please advise of an amount of hours as an integer number, which you would like to convert into correspondent amount of seconds: ");
 
-        while(isNumberCorrect){
+        while (isNumberCorrect) {
             Scanner scan = new Scanner(System.in);
-            try{
-                numberOfSeconds = hourToSeconds.getNumberOfSeconds(scan.nextInt());
-            }catch(IllegalArgumentException e){
-                numberOfSeconds = hourToSeconds.getNumberOfSeconds((scan.nextInt())*-1);
+            Integer hours = scan.nextInt();
+            try {
+                numberOfSeconds = hourToSeconds.getNumberOfSeconds(hours);
+            } catch (IllegalArgumentException e) {
+                numberOfSeconds = hourToSeconds.getNumberOfSeconds(hours * -1);
             }
-            isNumberCorrect = hours instanceof Integer ?false:true;
+            isNumberCorrect = hours instanceof Integer ? false : true;
         }
-       try{
-           System.out.println();
-       }
+        try {
+            System.out.println();
+        }
         HourToSeconds hourToSeconds = new HourToSeconds();
         Scanner scan = new Scanner(System.in);
         int result = hourToSeconds.getNumberOfSeconds(scan.nextInt());
