81e472aa543d3e12d52c54a37f89ffe18515adff
DorotaMiler <dorota.miler@outlook.com>
2020-03-09 23:50:00 +0100

    to be continued - error - no evaluation when negative number written from keyboards

--- src/main/java/_d_exceptions/_d_4_my_own_exceptions/_d_1_1_while_true/HourToSeconds.java
+++ src/main/java/_d_exceptions/_d_4_my_own_exceptions/_d_1_1_while_true/HourToSeconds.java
@@ -2,32 +2,32 @@ package _d_exceptions._d_4_my_own_exceptions._d_1_1_while_true;
 
 import java.util.Scanner;
 
+import static java.lang.Math.abs;
+
 public class HourToSeconds {
 
-    public Integer getNumberOfSeconds(Integer hour) throws IllegalArgumentException {
-        if (hour < 0) {
-            throw new IllegalArgumentException("Hour must be integer >= 0: (you wrote) " + hour + " -> please revise");
-        } else {
-            return hour * 60 * 60;
-        }
+    public Integer getNumberOfSeconds(Integer hour) {
+        return hour * 60 * 60;
     }
+
 
     public static void convertHourToSeconds() throws IllegalArgumentException {
         System.out.println("Please advise of an amount of hours as an integer number, which you would like to convert into correspondent amount of seconds: ");
-        Integer numberOfSeconds = 0;
-        Scanner scan = new Scanner(System.in);
-        Integer hours = scan.nextInt();
+        Integer numberOfSeconds = null;
+        Integer hours = null;
         boolean isNumberCorrect = true;
         HourToSeconds hourToSeconds = new HourToSeconds();
 
         while (isNumberCorrect) {
+            Scanner scan = new Scanner(System.in);
+            hours = scan.nextInt();
             try {
                 numberOfSeconds = hourToSeconds.getNumberOfSeconds(hours);
             } catch (IllegalArgumentException e) {
                 numberOfSeconds = hourToSeconds.getNumberOfSeconds(hours * -1);
             }
-            isNumberCorrect = hours < 0 ? true : false;
+//            isNumberCorrect = hours < 0 ? true : false;
         }
-        System.out.println("The result of converting " + hours + " hour(s) to seconds = " + numberOfSeconds);
+        System.out.println("The result of converting " + abs(hours) + " hour(s) to seconds = " + numberOfSeconds);
     }
 }
--- src/main/java/_d_exceptions/_d_4_my_own_exceptions/_d_1_1_while_true/NegativeOrZeroValueException.java
+++ src/main/java/_d_exceptions/_d_4_my_own_exceptions/_d_1_1_while_true/NegativeOrZeroValueException.java
@@ -1,4 +1,7 @@
 package _d_exceptions._d_4_my_own_exceptions._d_1_1_while_true;
 
-public class NegativeOrZeroValueException {
+public class NegativeOrZeroValueException extends Exception{
+    public NegativeOrZeroValueException(String message){
+        super(message);
+    }
 }
