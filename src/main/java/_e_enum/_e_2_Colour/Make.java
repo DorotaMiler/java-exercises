6c674459d09234f39a4af395562cfc0f5687d98c
DorotaMiler <dorota.miler@outlook.com>
2020-03-10 18:43:19 +0100

    addition of enums parameter (car makes model) + filed String model and adequate constructor

--- src/main/java/_e_enum/_e_2_Colour/Make.java
+++ src/main/java/_e_enum/_e_2_Colour/Make.java
@@ -1,10 +1,20 @@
 package _e_enum._e_2_Colour;
 
 public enum Make {
-    MERCEDES,
-    OPEL,
-    MAZDA,
-    FIAT,
-    BMW,
-    AUDI;
+    MERCEDES("GLA"),
+    OPEL("GRANDLAND X"),
+    MAZDA("CX-5"),
+    FIAT("500L"),
+    BMW("X7"),
+    AUDI("A3");
+
+    private String model;
+
+    Make(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
 }
