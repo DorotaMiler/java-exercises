9bb7a3c743dbec1a69f8f502fb6dae27a9a17ab0
DorotaMiler <dorota.miler@outlook.com>
2020-03-10 22:24:04 +0100

    localClassInstantiation(String[] args) addition

--- src/main/java/_f_classes/_f_1_inner_class/OuterClass2.java
+++ src/main/java/_f_classes/_f_1_inner_class/OuterClass2.java
@@ -1,5 +1,7 @@
 package _f_classes._f_1_inner_class;
 
+import java.util.Arrays;
+
 public class OuterClass2 {
 
     public static class InnerClass2 {
@@ -9,10 +11,20 @@ public class OuterClass2 {
         return new InnerClass2();
     }
 
-    public static void staticInnerClassInstantiation(){
+    public static void staticInnerClassInstantiation() {
         OuterClass2 outerClass2 = new OuterClass2();
         OuterClass2.InnerClass2 = outerClass2.instantiate();
         OuterClass2.InnerClass2 = new OuterClass2().InnerClass2();
     }
 
+    private static void localClassInstantiation(String[] args) {
+        class LocalClass {
+            @Override
+            public String toString() {
+                return "Argumenty metody: " + Arrays.toString(args);
+            }
+        }
+        LocalClass localClassInstance = new LocalClass();
+        System.out.println(localClassInstance);
+    }
 }
