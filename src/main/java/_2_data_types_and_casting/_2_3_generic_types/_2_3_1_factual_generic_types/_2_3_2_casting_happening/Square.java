b1536158e10c7db8a60a21240bf509dec810a14e
DorotaMiler <dorota.miler@outlook.com>
2020-03-11 23:15:59 +0100

    Rectangle implementing Figure & Square extending Rectangle for variation of results

---
+++ src/main/java/_2_data_types_and_casting/_2_3_generic_types/_2_3_1_factual_generic_types/_2_3_2_casting_happening/Square.java
@@ -0,0 +1,9 @@
+package _2_data_types_and_casting._2_3_generic_types._2_3_1_factual_generic_types._2_3_2_casting_happening;
+
+public class Square extends Rectangle{
+
+    @Override
+    public String getName() {
+        return "square";
+    }
+}
