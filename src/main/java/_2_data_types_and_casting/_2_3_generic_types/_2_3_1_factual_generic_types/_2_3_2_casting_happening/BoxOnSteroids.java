9478ad613fd5b964b227329da8f38b954651e120
DorotaMiler <dorota.miler@outlook.com>
2020-03-11 20:29:57 +0100

    comments addition for better uderstanding of how generic type works while getFruit() executed and reasing behind the <T> in class definition given

--- src/main/java/_2_data_types_and_casting/_2_3_generic_types/_2_3_1_factual_generic_types/_2_3_2_casting_happening/BoxOnSteroids.java
+++ src/main/java/_2_data_types_and_casting/_2_3_generic_types/_2_3_1_factual_generic_types/_2_3_2_casting_happening/BoxOnSteroids.java
@@ -1,13 +1,16 @@
 package _2_data_types_and_casting._2_3_generic_types._2_3_1_factual_generic_types._2_3_2_casting_happening;
 
+// below class definition with generic type (<T>):
 public class BoxOnSteroids<T> {
 
+    // T we can parameterize with a different type:
     private T fruit;
 
     public BoxOnSteroids(T fruit) {
         this.fruit = fruit;
     }
 
+    // compiler detects an exact return Object type with getFruit():
     public T getFruit() {
         return fruit;
     }
