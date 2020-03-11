c098c234a082027d1e71b98647b2e78a562b8ba2
DorotaMiler <dorota.miler@outlook.com>
2020-03-11 21:58:23 +0100

    Pair.java with two generic types

---
+++ src/main/java/_2_data_types_and_casting/_2_3_generic_types/_2_3_1_factual_generic_types/_2_3_2_casting_happening/Pair.java
@@ -0,0 +1,18 @@
+package _2_data_types_and_casting._2_3_generic_types._2_3_1_factual_generic_types._2_3_2_casting_happening;
+
+public class Pair<T, S> {
+    private T first;
+    private S second;
+
+    public Pair(T first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+    }
+}
