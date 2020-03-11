068a114a863acf902529ca8610dd8918b3d7fdfc
DorotaMiler <dorota.miler@outlook.com>
2020-03-11 23:11:57 +0100

    circleBox creation basing on BoxForFigures

--- src/main/java/_2_data_types_and_casting/_2_3_generic_types/_2_3_1_factual_generic_types/_2_3_2_casting_happening/BoxForFigures.java
+++ src/main/java/_2_data_types_and_casting/_2_3_generic_types/_2_3_1_factual_generic_types/_2_3_2_casting_happening/BoxForFigures.java
@@ -1,4 +1,17 @@
 package _2_data_types_and_casting._2_3_generic_types._2_3_1_factual_generic_types._2_3_2_casting_happening;
 
-public class BoxForFigures {
+public class BoxForFigures <T extends Circle>{
+    private T element;
+
+    public BoxForFigures(T element){
+        this.element = element;
+    }
+
+    public T getElement() {
+        return element;
+    }
+
+    public String getElementName(){
+        return element.getName();
+    }
 }
--- src/main/java/_2_data_types_and_casting/_2_3_generic_types/_2_3_1_factual_generic_types/_2_3_2_casting_happening/Main.java
+++ src/main/java/_2_data_types_and_casting/_2_3_generic_types/_2_3_1_factual_generic_types/_2_3_2_casting_happening/Main.java
@@ -1,21 +1,28 @@
 package _2_data_types_and_casting._2_3_generic_types._2_3_1_factual_generic_types._2_3_2_casting_happening;
 
-import javax.swing.*;
-
 public class Main {
 
     public static void main(String[] args) {
+
+        //  Orange, Apple, FruitBox:
         FruitBox fruitBox = new FruitBox(new Orange());
         Orange fruit1 = (Orange) fruitBox.getFruit();
         System.out.println("First orange generated from FruitBox class: " + fruit1.getClass());
 
-        BoxOnSteroids<Apple> applebox= new BoxOnSteroids<>(new Apple());
+        //  BoxOnStreroids, Apple, Orange
+        BoxOnSteroids<Apple> applebox = new BoxOnSteroids<>(new Apple());
         //  the above same as below:
         //  BoxOnSteroids<Apple> appleBox = new BoxOnSteroids<Apple>(new Apple());
         BoxOnSteroids<Orange> orangeBox = new BoxOnSteroids<>(new Orange());
         Orange orange = orangeBox.getFruit();
         System.out.println("Orange from the orange box which is on steroids is: " + orange);
 
-        Pair<BoxOnSteroids<Orange>,BoxOnSteroids<Apple>> pairOfBoxes = new Pair<>(new BoxOnSteroids<>(new Orange()), new BoxOnSteroids<>(new Apple()));
+        Pair<BoxOnSteroids<Orange>, BoxOnSteroids<Apple>> pairOfBoxes = new Pair<>(new BoxOnSteroids<>(new Orange()), new BoxOnSteroids<>(new Apple()));
+
+        //  Figure, Circle & BoxForFigures:
+        BoxForFigures<Circle> circleBox = new BoxForFigures<>(new Circle());
+        //  BoxForFigures<Apple>applebox //  compilation error
+
+
     }
 }
