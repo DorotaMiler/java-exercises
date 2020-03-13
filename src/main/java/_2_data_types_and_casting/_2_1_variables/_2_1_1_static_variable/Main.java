fadfeea9253ad56f91e2773b2285323ca3d775fa
DorotaMiler <dorota.miler@outlook.com>
2020-03-13 21:36:59 +0100

    Car.numberOfCars enhancement -> eassen code (unnecessary code under comment)

--- src/main/java/_2_data_types_and_casting/_2_1_variables/_2_1_1_static_variable/Main.java
+++ src/main/java/_2_data_types_and_casting/_2_1_variables/_2_1_1_static_variable/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
 
         Car car1 = new Car("Mercedes", "Diesel");
-        int currentAmount = Car.numberOfCars;
-        System.out.println("1st car's info: \n" + " -> make: " + car1.getMake() + "\n -> engine: " + car1.getEngine() + "\n    * current amount of cars in record: " + currentAmount);
+        //  int currentAmount = Car.numberOfCars;
+        System.out.println("1st car's info: \n" + " -> make: " + car1.getMake() + "\n -> engine: " + car1.getEngine() + "\n    * current amount of cars in record: " + Car.numberOfCars);
         Car car2 = new Car("Toyota", "Petrol");
-        currentAmount = Car.numberOfCars;
-        System.out.println("2nd car's info: \n" + " -> make: " + car2.getMake() + "\n -> engine: " + car2.getEngine() + "\n    * current amount of cars in record: " + currentAmount);
+        //  currentAmount = Car.numberOfCars;
+        System.out.println("2nd car's info: \n" + " -> make: " + car2.getMake() + "\n -> engine: " + car2.getEngine() + "\n    * current amount of cars in record: " + Car.numberOfCars);
     }
 }
