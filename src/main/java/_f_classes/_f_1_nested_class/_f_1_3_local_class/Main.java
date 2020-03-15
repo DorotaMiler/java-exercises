ee7170953b2058776d054d37ad10401409a70a1e
DorotaMiler <dorota.miler@outlook.com>
2020-03-15 23:14:38 +0100

    Main addition envoking NewEnclosingClass object & executing addCounts() method

--- src/main/java/_f_classes/_f_1_nested_class/_f_1_3_local_class/Main.java
+++ src/main/java/_f_classes/_f_1_nested_class/_f_1_3_local_class/Main.java
@@ -1,4 +1,8 @@
 package _f_classes._f_1_nested_class._f_1_3_local_class;
 
 public class Main {
+    public static void main(String[] args) {
+        NewEnclosingClass nec = new NewEnclosingClass();
+        nec.addCounts(); // prints 6
+    }
 }
