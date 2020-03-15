e3d3df5704aba67fb87363c4af5cdb7e9c3cba36
DorotaMiler <dorota.miler@outlook.com>
2020-03-15 22:35:59 +0100

    static block with exemplary static List creation

---
+++ src/main/java/_f_classes/_f_1_nested_class/_f_5_static_block/StaticBlockExample.java
@@ -0,0 +1,29 @@
+package _f_classes._f_1_nested_class._f_5_static_block;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class StaticBlockExample {
+
+    //  initializing a list object with pre-defined values
+    // (If static variables require additional, multi-statement
+    // logic while initialization, then a static block can be used)
+
+    public static List<String> ranks = new LinkedList<>();
+
+    static{
+        ranks.add("Lieutenant");
+        ranks.add("Captain");
+        ranks.add("Major");
+    }
+
+    static{
+        ranks.add("Colonel");
+        ranks.add("General");
+    }
+
+    //  it wouldn't be possible to initialize List object
+    //  with all the initial values along with declaration;
+    //  that's why utilized the static block here
+
+}
