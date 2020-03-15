f161ead21aa0d75d696eb81b095ab5593600836d
DorotaMiler <dorota.miler@outlook.com>
2020-03-15 23:34:36 +0100

    comment addition to anonymous class

--- src/main/java/_f_classes/_f_1_nested_class/_f_1_4_anonymous_class/Main.java
+++ src/main/java/_f_classes/_f_1_nested_class/_f_1_4_anonymous_class/Main.java
@@ -2,6 +2,13 @@ package _f_classes._f_1_nested_class._f_1_4_anonymous_class;
 
 public class Main {
 
+    // -> utilization of the anonymous class when you need functionality
+    // but not necessarily a separate entity.
+
+    // Creating a new class that implements the Math
+    // interface would be overkill (especially since
+    // using the math variable in only one place).
+
     public static void main(String[] args) {
         AnonymousExample ae = new AnonymousExample();
         ae.math.add();  //  result = 6
