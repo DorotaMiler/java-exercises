4f32bc83d1a9f7b9117a7e2b2b9a764226622b08
DorotaMiler <dorota.miler@outlook.com>
2020-03-18 23:24:34 +0100

    creation of enum fields (add, substract, devide & multiply)+private field sign

--- src/main/java/_9_small_projects/EnumApp/MathOperations.java
+++ src/main/java/_9_small_projects/EnumApp/MathOperations.java
@@ -1,4 +1,14 @@
 package _9_small_projects.EnumApp;
 
 public enum MathOperations {
+    ADD("+",),
+    SUBSTRACT("-"),
+    MULTIPLY("*"),
+    DEVIDE("/");
+
+    private String sign;
+
+    MathOperations(String sign){
+        this.sign = sign;
+    }
 }
