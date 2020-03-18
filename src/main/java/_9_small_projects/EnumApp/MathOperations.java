8dae6a6a948776a76852e596331fff86b9be188a
DorotaMiler <dorota.miler@outlook.com>
2020-03-18 23:30:32 +0100

    new filed -> optionNumber addition for better methods viewMathOperations() overview

--- src/main/java/_9_small_projects/EnumApp/MathOperations.java
+++ src/main/java/_9_small_projects/EnumApp/MathOperations.java
@@ -1,14 +1,21 @@
 package _9_small_projects.EnumApp;
 
 public enum MathOperations {
-    ADD("+",),
-    SUBSTRACT("-"),
-    MULTIPLY("*"),
-    DEVIDE("/");
+    ADD(1, "+"),
+    SUBSTRACT(2, "-"),
+    MULTIPLY(3, "*"),
+    DEVIDE(4, "/");
 
     private String sign;
+    private Integer optionNumber;
 
-    MathOperations(String sign){
+    MathOperations(Integer optionNumber, String sign) {
+        this.optionNumber = optionNumber;
         this.sign = sign;
+    }
+
+    public viewMathOpearations() {
+        System.out.println("Available math operations (in enum form): \n");
+        System.out.println();
     }
 }
