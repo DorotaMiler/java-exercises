ed32a936a3e623792918d0d4051a3594a9c5dfb3
DorotaMiler <dorota.miler@outlook.com>
2020-03-19 00:05:11 +0100

    for better overview -> RunMathOperations class addition and move of so far created methods

--- src/main/java/_9_small_projects/EnumApp/MathOperations.java
+++ src/main/java/_9_small_projects/EnumApp/MathOperations.java
@@ -1,7 +1,5 @@
 package _9_small_projects.EnumApp;
 
-import java.util.Scanner;
-
 public enum MathOperations {
     ADD(1, "+"),
     SUBSTRACT(2, "-"),
@@ -14,43 +12,5 @@ public enum MathOperations {
     MathOperations(Integer optionNumber, String sign) {
         this.optionNumber = optionNumber;
         this.sign = sign;
-    }
-
-    public void viewMathOpearations() {
-        System.out.println("------------------------------------------------------");
-        System.out.println("-----  Available math operations (in enum form): -----\n");
-        System.out.println("***    1. Add ( + )                                ***");
-        System.out.println("***    1. Substract ( - )                          ***");
-        System.out.println("***    1. Multiply ( * )                           ***");
-        System.out.println("***    1. Devide ( / )                             ***");
-        System.out.println("-------------------------------------------------------");
-    }
-
-    public void chooseMathOperation() {
-        System.out.println("Please choose from the below available 4 options:");
-        viewMathOpearations();
-     int chosenOption = getConsoleInput();
-        switch (chosenOption) {
-            case 1:
-                System.out.println("Choice confirmation: " + MathOperations.ADD.optionNumber + " implies -> " + MathOperations.ADD.sign);
-                break;
-            case 2:
-                System.out.println("Choice confirmation: " + MathOperations.SUBSTRACT.optionNumber + " implies -> " + MathOperations.SUBSTRACT.sign);
-                break;
-            case 3:
-                System.out.println("Choice confirmation: " + MathOperations.MULTIPLY.optionNumber + " implies -> " + MathOperations.MULTIPLY.sign);
-                break;
-            case 4:
-                System.out.println("Choice confirmation: " + MathOperations.DEVIDE.optionNumber + " implies -> " + MathOperations.DEVIDE.sign);
-                break;
-            default:
-                System.out.println("Chosen option incorrect -> please try again (choose integer ranging from 1 -4)");
-        }
-    }
-
-    public int getConsoleInput(){
-        Scanner scan = new Scanner(System.in);
-        int chosenOption = scan.nextInt();
-        return chosenOption;
     }
 }
