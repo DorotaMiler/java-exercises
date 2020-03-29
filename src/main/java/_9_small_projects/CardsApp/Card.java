1b27b40dd88da64a0dc06a906cf3335e7df69bcd
DorotaMiler <dorota.miler@outlook.com>
2020-03-30 00:56:58 +0200

    Card class with enum fields

---
+++ src/main/java/_9_small_projects/CardsApp/Card.java
@@ -0,0 +1,14 @@
+package _9_small_projects.CardsApp;
+
+public class Card {
+    private Rank rank;
+    private Suit suit;
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+}
