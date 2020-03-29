abc69335ae585764dcb605483c2afcf126cbe7ab
DorotaMiler <dorota.miler@outlook.com>
2020-03-29 18:57:04 +0200

    Main -> showcasing methods work

--- src/main/java/_b_inheritance/_b_5_super_and_this/_b_5_2_animals/Main.java
+++ src/main/java/_b_inheritance/_b_5_super_and_this/_b_5_2_animals/Main.java
@@ -1,4 +1,10 @@
 package _b_inheritance._b_5_super_and_this._b_5_2_animals;
 
 public class Main {
+    public static void main(String[] args) {
+        // below Animal as reference; Dog as an abject implication
+        Animal dog = new Dog("Reksio");
+        dog.printName();
+        dog.move();
+    }
 }
