01abfde15c573ead7515c74315e87e1277b2d8c3
DorotaMiler <dorota.miler@outlook.com>
2020-03-29 18:58:53 +0200

    link addition in comment for reference

--- src/main/java/_b_inheritance/_b_5_super_and_this/_b_5_2_animals/Main.java
+++ src/main/java/_b_inheritance/_b_5_super_and_this/_b_5_2_animals/Main.java
@@ -1,5 +1,8 @@
 package _b_inheritance._b_5_super_and_this._b_5_2_animals;
 
+//  cool insight:
+// https://howtodoinjava.com/java/basics/this-vs-super/
+
 public class Main {
     public static void main(String[] args) {
         // below Animal as reference; Dog as an abject implication
