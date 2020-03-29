257dac67e9a6abdafbebf70c0f0e17aa3315abef
DorotaMiler <dorota.miler@outlook.com>
2020-03-30 00:49:27 +0200

    removed legs funtionality from Dog class to avid confution with doubled this. within same constructor

--- src/main/java/_b_inheritance/_b_5_super_and_this/_b_5_2_animals/Dog.java
+++ src/main/java/_b_inheritance/_b_5_super_and_this/_b_5_2_animals/Dog.java
@@ -3,7 +3,6 @@ package _b_inheritance._b_5_super_and_this._b_5_2_animals;
 public class Dog extends Animal {
 
     Dog(String name) {
-
         //  can use this or super only once in constructor
         //  also this cannot be followed by super and vise versa
         //  -> choose one of them
@@ -13,12 +12,7 @@ public class Dog extends Animal {
     Dog() {
         //  this should be ALWAYS the first statement!
         //  - otherwise error
-        this(4);
         this("test");
-    }
-
-    Dog(int legs) {
-        System.out.println("Dog has " + legs + " legs.");
     }
 
     @Override
