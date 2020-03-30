2476e356a84c168a47301f63004871018075f874
DorotaMiler <dorota.miler@outlook.com>
2020-03-30 23:20:01 +0200

    suitChoice() addition

--- src/main/java/_9_small_projects/CardsApp/Card.java
+++ src/main/java/_9_small_projects/CardsApp/Card.java
@@ -17,8 +17,9 @@ public class Card {
 
     }
 
-    public void rankChoice() {
+    public void suitChoice() {
         System.out.println("Please follow below instructions:\n");
+        System.out.println("--------------------------------------------------------");
         System.out.println("--------------------------------------------------------");
         System.out.println("-----  Please choose one suit from below 4 options:-----\n");
         System.out.println("***    1. Clubs                                      ***");
@@ -26,6 +27,18 @@ public class Card {
         System.out.println("***    3. Hearts                                     ***");
         System.out.println("***    4. Spades                                     ***");
         System.out.println("---------------------------------------------------------");
+    }
+
+    public void rankChoice() {
+        System.out.println(":) thank you\n");
+        System.out.println("------------------------------------------------------------");
+        System.out.println("-----  Now please choose one rank from below 4 options:-----\n");
+        System.out.println("***    1. Ace                                            ***");
+        System.out.println("***    2. King                                           ***");
+        System.out.println("***    3. Queen                                          ***");
+        System.out.println("***    4. Jack                                           ***");
+        System.out.println("------------------------------------------------------------");
+        System.out.println("------------------------------------------------------------");
     }
 
 }
