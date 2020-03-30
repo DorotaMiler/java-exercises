d51a09b0da64168bef1385f6fefec7166ce1a250
DorotaMiler <dorota.miler@outlook.com>
2020-03-31 00:20:36 +0200

    attempt to create ethods returning chosen ranks and suits in a String form to utilize StringJoiner in the end to showcase result

--- src/main/java/_9_small_projects/CardsApp/Card.java
+++ src/main/java/_9_small_projects/CardsApp/Card.java
@@ -1,6 +1,7 @@
 package _9_small_projects.CardsApp;
 
 import java.util.Scanner;
+import java.util.StringJoiner;
 
 public class Card {
     private Rank rank;
@@ -14,12 +15,58 @@ public class Card {
         return suit;
     }
 
-    getDescription(Rank rank, Suit suit) {
+    public String getDescription(Rank rank, Suit suit) {
+
+        StringJoiner joiner = new StringJoiner(" - ", "{", "}");
+        String result = joiner.add("Dorota").add("is").add("ill").add("and").add("stays").add("home").toString();
+        System.out.println(result);
+    }
+
+
+    public String suitChoice() {
+        suitMenu();
+
+        boolean isCorrect = true;
+        while (isCorrect) {
+            int firstChoice = scanChoice();
+            switch (firstChoice) {
+                case 1:
+                    return Suit.CLUBS.name();
+                    break;
+                case 2:
+                    return Suit.DIAMONDS.name();
+                    break;
+                case 3:
+                    return Suit.HEARTS.name();
+                    break;
+                case 4:
+                    return Suit.SPADES.name();
+                    break;
+
+            }
+        }
+        return (rank.toString() + " - " + suit.name().)
+    }
+
+
+    getRankChoice() {
+        rankMenu();
+        int secondChoice = scanChoice();
+        boolean isCorrect = true;
+        while (isCorrect) {
+
+
+            swich(secondChoice) {
+                case 1:
+
+            }
+
+        }
         return (rank.toString() + " - " + suit.name().)
 
     }
 
-    public void suitChoice() {
+    public void suitMenu() {
         System.out.println("Please follow below instructions:\n");
         System.out.println("--------------------------------------------------------");
         System.out.println("--------------------------------------------------------");
@@ -31,7 +78,7 @@ public class Card {
         System.out.println("---------------------------------------------------------");
     }
 
-    public void rankChoice() {
+    public void rankMenu() {
         System.out.println(":) thank you\n");
         System.out.println("------------------------------------------------------------");
         System.out.println("-----  Now please choose one rank from below 4 options:-----\n");
@@ -43,16 +90,16 @@ public class Card {
         System.out.println("------------------------------------------------------------");
     }
 
-public int scanChoice(){
+    public int scanChoice() {
         Scanner scan = new Scanner(System.in);
         int chosenOption = scan.nextInt();
         return chosenOption;
-}
+    }
 
-@Override
+    @Override
     public String toString() {
         return Suit.
-                "ran=" + size +
+        "ran=" + size +
                 ", manufacturer='" + manufacturer + '\'' +
                 '}';
     }
