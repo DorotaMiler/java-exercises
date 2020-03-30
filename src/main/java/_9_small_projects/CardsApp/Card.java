fbf2c5547e291445568f1210e72680fc66b821c0
DorotaMiler <dorota.miler@outlook.com>
2020-03-30 23:15:59 +0200

    menu() creation for user to choose from available ranks

--- src/main/java/_9_small_projects/CardsApp/Card.java
+++ src/main/java/_9_small_projects/CardsApp/Card.java
@@ -11,4 +11,21 @@ public class Card {
     public Suit getSuit() {
         return suit;
     }
+
+    getDescription(Rank rank, Suit suit) {
+        return (rank.toString() + " - " + suit.name().)
+
+    }
+
+    public void menu() {
+        System.out.println("Please follow below instructions:\n");
+        System.out.println("--------------------------------------------------------");
+        System.out.println("-----  Please choose one suit from below 4 options:-----\n");
+        System.out.println("***    1. Clubs                                      ***");
+        System.out.println("***    2. Diamonds                                   ***");
+        System.out.println("***    3. Hearts                                     ***");
+        System.out.println("***    4. Spades                                     ***");
+        System.out.println("---------------------------------------------------------");
+    }
+
 }
