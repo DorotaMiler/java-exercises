b22a957e5b9c0cbb975692f68938d38fa4057f0e
DorotaMiler <dorota.miler@outlook.com>
2020-04-08 23:42:03 +0200

    enum exercise conduction with Envirnonmental urls

--- src/main/java/_a_object_programming/_a_2_constructor/Connection/Main.java
+++ src/main/java/_a_object_programming/_a_2_constructor/Connection/Main.java
@@ -3,6 +3,6 @@ package _a_object_programming._a_2_constructor.Connection;
 public class Main {
 
     public static void main(String[] args) {
-        new ConnectionConstr("url", "Dori", {a,b,b,a});
+        new ConnectionConstr("url", "Dori", {a, b, b, a});
     }
 }
---
+++ src/main/java/_e_enum/_e_3_Environment/Environment.java
@@ -0,0 +1,17 @@
+package _e_enum._e_3_Environment;
+
+public enum Environment {
+    PROD("https://prod.domain.com:1088/"),
+    SIT("https://sit.domain.com:2019/"),
+    CIT("https://cit.domain.com:8080/"),
+    DEV("https://dev.domain.com:21323/");
+
+    private String url;
+    Environment(String url){
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
