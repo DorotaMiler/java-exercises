cc676bbffae0acd333e91dffb6a980e00e2721da
DorotaMiler <dorota.miler@outlook.com>
2020-04-08 19:07:24 +0200

    idea of basic concept of <T> type

--- src/main/java/_2_data_types_and_casting/_2_3_generic_types/_2_3_3_Calculator/CalculatorImpl.java
+++ src/main/java/_2_data_types_and_casting/_2_3_generic_types/_2_3_3_Calculator/CalculatorImpl.java
@@ -1,4 +1,23 @@
 package _2_data_types_and_casting._2_3_generic_types._2_3_3_Calculator;
 
-public class CalculatorImpl {
+public class CalculatorImpl implements Calculator<Double> {
+    @Override
+    public double add(Double... args) {
+        return 0;
+    }
+
+    @Override
+    public double subtract(Double... args) {
+        return 0;
+    }
+
+    @Override
+    public double multiply(Double... args) {
+        return 0;
+    }
+
+    @Override
+    public double divide(Double... args) {
+        return 0;
+    }
 }
