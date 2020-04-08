fafbdc7e3078377433eec797c4d9c4071e08d9d0
DorotaMiler <dorota.miler@outlook.com>
2020-04-08 22:05:58 +0200

    GenericEx extention -> few more methods for practice

--- src/main/java/_2_data_types_and_casting/_2_3_generic_types/_2_3_1_basic_concept/GenericEx.java
+++ src/main/java/_2_data_types_and_casting/_2_3_generic_types/_2_3_1_basic_concept/GenericEx.java
@@ -4,17 +4,28 @@ public class GenericEx<T> {
 
     public T value;
 
-    public GenericEx(T value){
+    public GenericEx(T value) {
         this.value = value;
     }
 
-    public <T> String print(T first, T second){
-        System.out.println(first.getClass());
-        System.out.println(second.getClass());
-        return first.toString();
+    public <T> String print(T a, T b) {
+        System.out.println(a.getClass());
+        System.out.println(b.getClass());
+        return a.toString();
     }
 
-    public <T, Y extends Person> void testMethod (T a, Y b){
+    public <T, Y extends Person> void testMethod(T a, Y b) {
     }
 
+    public <T extends Number> double add(T a, T b) {
+        return a.doubleValue() + b.doubleValue();
+    }
+
+    public <T extends Number> double add(T... numbers) {
+        double result;
+        for (T number : numbers) {
+            result += number.doubleValue();
+        }
+        return result;
+    }
 }
---
+++ src/main/java/_2_data_types_and_casting/_2_3_generic_types/_2_3_1_basic_concept/Person.java
@@ -0,0 +1,86 @@
+package _2_data_types_and_casting._2_3_generic_types._2_3_1_basic_concept;
+
+/*
+
+Creation of a class containing:
+3 fileds
+2 methods: one returning a value, void, getters/setters,
+equals() & hashcode()
+ */
+
+import java.util.Objects;
+
+public class Person {
+    String name;
+    byte age;
+    String pesel;
+
+    public Person(byte age) {
+        pesel = "";
+        this.age = age;
+    }
+
+    public Person(byte age, String pesel) {
+        this.age = age;
+        this.pesel = pesel;
+    }
+
+    public Person(String name, byte age, String pesel) {
+        this.name = name;
+        this.age = age;
+        this.pesel = pesel;
+    }
+
+    public void whoAmI(String name, byte age) {
+        System.out.println("You are " + name + " and you are " + age + " years old");
+    }
+
+    public String getMonth() {
+        return pesel.substring(2, 3);
+    }
+
+    public boolean isPeselValid() {
+        return !pesel.isEmpty();
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(long pesel) {
+        this.pesel = String.valueOf(pesel);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Person{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", age=").append(age);
+        sb.append(", pesel='").append(pesel).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+        Person person = (Person) o;
+        return age == person.age &&
+                getName().equals(person.getName()) &&
+                getPesel().equals(person.getPesel());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), age, getPesel());
+    }
+}
