5f5d7e38fcd5d7e96bff6d151df0fd4cc74debce
DorotaMiler <dorota.miler@outlook.com>
2020-04-08 20:40:55 +0200

    creation of Person -> with pesel creation/setup

---
+++ src/main/java/_1_HelloWorld/Person.java
@@ -0,0 +1,61 @@
+package _1_HelloWorld;
+
+/*
+
+Creation of a class containing:
+3 fileds
+2 methods: one returning a value, void, getters/setters,
+equals() & hashcode()
+ */
+
+import static jdk.nashorn.internal.objects.NativeString.substring;
+
+public class Person {
+    String name;
+    byte age;
+    String pesel;
+
+    public Person(byte age) {
+        pesel ="";
+        this.age = age;
+    }
+
+    public Person(byte age, String pesel){
+        this.age = age;
+        this.pesel = pesel;
+    }
+
+    public Person(String name, byte age, String pesel) {
+        this.name = name;
+        this.age = age;
+        this.pesel = pesel;
+    }
+
+    public void whoAmI(String name, byte age) {
+        System.out.println("You are " + name + " and you are " + age + " years old");
+    }
+
+    public String getMonth(){
+        return pesel.substring(2,3);
+    }
+
+    public boolean isPeselValid(){
+        return !pesel.isEmpty();
+    }
+
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(long pesel) {
+        this.pesel = String.valueOf(pesel);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
