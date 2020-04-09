6d2a68896332b4bf6d84879f3da067f3f12df1be
DorotaMiler <dorota.miler@outlook.com>
2020-04-09 19:11:35 +0200

    new package - data structures

---
+++ src/main/java/_g_data_structures/_g_1_tables/Main.java
@@ -0,0 +1,9 @@
+package _g_data_structures._g_1_tables;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] a = new int[100];
+        long b[] = new long[10;
+        String[] c = new String[5];]
+    }
+}
---
+++ src/main/java/_g_data_structures/_g_1_tables/TableEx.java
@@ -0,0 +1,7 @@
+package _g_data_structures._g_1_tables;
+
+public class TableEx {
+    int[] a;
+    long[] b;
+    String[] c;
+}
