38fa05a9bdc0700d6645995f2d1fd5a54f5f21b8
DorotaMiler <dorota.miler@outlook.com>
2020-04-09 23:56:54 +0200

    List example

---
+++ src/main/java/_g_data_structures/_g_2_Collections/ListEx.java
@@ -0,0 +1,13 @@
+package _g_data_structures._g_2_Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListEx {
+
+    List<Integer> list = new ArrayList<>();
+    list.add(5);
+    list.add.(4567);
+    list.add.(-87);
+
+}
