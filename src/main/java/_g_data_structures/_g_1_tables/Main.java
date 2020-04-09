28019c31171c7f346c9cf6c1d6272a1f284443b2
DorotaMiler <dorota.miler@outlook.com>
2020-04-09 23:44:21 +0200

    few examples of code on tables

--- src/main/java/_g_data_structures/_g_1_tables/Main.java
+++ src/main/java/_g_data_structures/_g_1_tables/Main.java
@@ -3,7 +3,29 @@ package _g_data_structures._g_1_tables;
 public class Main {
     public static void main(String[] args) {
         int[] a = new int[100];
-        long b[] = new long[10;
-        String[] c = new String[5];]
+        long b[] = new long[10];
+        String[] c = new String[5];
+
+        a[0] = 5;
+        a[1] = 245;
+        a[2] = -27;
+
+        c[0] = "Ala";
+        c[2] = "ma";
+        c[4] = "kota";
+
+        public void fillAValues() {
+            for (var i = 0; i < 100; i++) {
+                a[i] = i;
+                System.out.println(a[i]);
+            }
+        }
+
+        public void fillCValues() {
+            for (var i = 0; i < 5; i++) {
+                c[i] = i;
+                System.out.println(c[i]);
+            }
+        }
     }
 }
--- src/main/java/_g_data_structures/_g_1_tables/TableEx.java
+++ src/main/java/_g_data_structures/_g_1_tables/TableEx.java
@@ -4,4 +4,9 @@ public class TableEx {
     int[] a;
     long[] b;
     String[] c;
+
+    public void printCElement(String[] c){
+        System.out.println("Element nr 0 from c table is: "+c[0]);
+    }
+
 }
