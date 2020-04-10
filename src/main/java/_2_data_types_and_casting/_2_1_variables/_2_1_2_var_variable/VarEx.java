1c87287dea3ee467ddb2272de58f9dc8f81b6307
DorotaMiler <dorota.miler@outlook.com>
2020-04-10 23:06:50 +0200

    var variable example -> to continue in data structures package

---
+++ src/main/java/_2_data_types_and_casting/_2_1_variables/_2_1_2_var_variable/VarEx.java
@@ -0,0 +1,20 @@
+package _2_data_types_and_casting._2_1_variables._2_1_2_var_variable;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class VarEx {
+
+    /*
+    var is a reserved name of
+    data type (not a key word as int, void etc.) for
+    long-worded objects to shorten the local variables'
+    declaration
+    * only as a LOCAL variable
+    */
+
+    public void readFile() {
+        var fileR = new FileReader("plik.txt");
+        var reader = new BufferedReader(fileR);
+    }
+}
