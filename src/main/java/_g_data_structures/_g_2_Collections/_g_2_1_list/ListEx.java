d7c03d541e6091768b781652633b35b6b36b4c49
DorotaMiler <dorota.miler@outlook.com>
2020-04-10 23:39:03 +0200

    List example

--- src/main/java/_g_data_structures/_g_2_Collections/_g_2_1_list/ListEx.java
+++ src/main/java/_g_data_structures/_g_2_Collections/_g_2_1_list/ListEx.java
@@ -1,10 +1,10 @@
-package _g_data_structures._g_2_Collections;
+package _g_data_structures._g_2_Collections._g_2_1_list;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListEx {
-
+    // .add adds at the end of a list
     List<Integer> list = new ArrayList<>();
     list.add(5);
     list.add.(4567);
