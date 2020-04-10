1918f6f5a1a582091464ab4eaca54c1a5812330f
DorotaMiler <dorota.miler@outlook.com>
2020-04-10 23:13:12 +0200

    variable arity example

---
+++ src/main/java/_2_data_types_and_casting/_2_1_variables/varargs/VarArgsEx.java
@@ -0,0 +1,13 @@
+package _2_data_types_and_casting._2_1_variables.varargs;
+
+public class VarArgsEx {
+    // variable arguments or variable arity
+    public static void main(String[] args) {
+        int sum ( int ...numbers){
+        }
+
+        int sum ( int[] numbers){
+
+        }
+    }
+}
