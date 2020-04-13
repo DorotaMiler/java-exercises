7bcf2ddc06396b1a2792f685ae1f7e40008bffc6
DorotaMiler <dorota.miler@outlook.com>
2020-04-13 19:14:02 +0200

    Array class

---
+++ src/main/java/_f_classes/_f_5_Arrays/Worker.java
@@ -0,0 +1,34 @@
+package _f_classes._f_5_Arrays;
+
+public class Worker {
+    private String name;
+    private String lastName;
+    private double payout;
+
+    public Worker(String name, String lastName, double payout){
+        this.name=name;
+        this.lastName = lastName;
+        this.payout=payout;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public double getPayout() {
+        return payout;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", payout=" + payout +
+                '}';
+    }
+}
