6494580fa7b1873c543fce563965bddfaf44df93
DorotaMiler <dorota.miler@outlook.com>
2020-04-14 23:55:31 +0200

    MapEx -> .put adding values of a specific key

---
+++ src/main/java/_g_data_structures/_g_2_Collections/_g_2_1_list/MapEx.java
@@ -0,0 +1,11 @@
+package _g_data_structures._g_2_Collections._g_2_1_list;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapEx {
+    public static void main(String[] args) {
+        Map<Integer, String> employees = new TreeMap<>();
+        employees.put(11,"DorotaMiler");
+    }
+}
