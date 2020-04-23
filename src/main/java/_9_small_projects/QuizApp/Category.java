cb30baa923bafe5a39f9b6e5364dcbd4ca8d2b4c
DorotaMiler <dorota.miler@outlook.com>
2020-04-23 23:49:48 +0200

    Category as enum - naming of 3

--- src/main/java/_9_small_projects/QuizApp/Category.java
+++ src/main/java/_9_small_projects/QuizApp/Category.java
@@ -1,4 +1,23 @@
 package _9_small_projects.QuizApp;
 
 public enum Category {
+    TECHNOLOGY(1),
+    BIOLOGY(2),
+    COVID19;
+
+    private Integer order;
+
+    public Category(Integer order) {
+        this.order = order;
+    }
+
+    public Integer getOrder() {
+        return order;
+    }
+
+    public void iterateCategory() {
+        for (Category categories : Category.values()) {
+            System.out.println(categories.getOrder() + " : " + categories.name());
+        }
+    }
 }
--- src/main/java/_9_small_projects/QuizApp/Question.java
+++ src/main/java/_9_small_projects/QuizApp/Question.java
@@ -1,4 +1,20 @@
 package _9_small_projects.QuizApp;
 
+import _c_types_comparison._c_1_hashcode.HashTest;
+
+import java.util.Set;
+
 public class Question {
+
+    Set<Question> allQuestions = HashTest<>();
+
+    public void question1{
+        System.out.println("What does “HTTP” stand for?");
+        System.out.println();
+        System.out.println("a.HyperText Transfer Protocol:);
+                System.out.println("b. ksachbds");
+        System.out.println("c. dsfgsth");
+        System.out.println("d. fbmdxsdfb");
+
+    }
 }
