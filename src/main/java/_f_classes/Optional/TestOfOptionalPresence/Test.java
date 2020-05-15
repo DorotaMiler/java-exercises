3d00cdeb41ce9d143a689b692df2c23a4cbc63cd
DorotaMiler <dorota.miler@outlook.com>
2020-05-15 23:13:06 +0200

    showcasing how profitable it is to utilize .ofNullable to avoid NullPointerException

--- src/main/java/_f_classes/Optional/TestOfOptionalPresence/Test.java
+++ src/main/java/_f_classes/Optional/TestOfOptionalPresence/Test.java
@@ -4,7 +4,25 @@ import java.util.Optional;
 
 public class Test {
     public static void main(String[] args) {
+
+        // empty optional heck:
         Optional<String> empty = Optional.empty();
+        empty.isPresent(); // false
+        empty.isEmpty(); // true
+
+
+        // not empty optional check:
+        Optional<String> notEmpty = Optional.of("Not empty");
+        notEmpty.isEmpty(); // false
+        notEmpty.isPresent(); // true
+
+        //  NullPointerException:
+        String value = null;
+        Optional.of(value);
+
+        // it is safr to utiliza .ofNullable to avoid exception:
+        String value1 = null;
+        Optional<String>empty = Optional.ofNullable(value);
         empty.isPresent(); // false
         empty.isEmpty(); // true
     }
