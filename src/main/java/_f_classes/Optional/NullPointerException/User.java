14dd208959057b85e077e60e87f0adeb3aae6b93
DorotaMiler <dorota.miler@outlook.com>
2020-05-18 21:25:00 +0200

    pom.xml modification - UTF-8 addition + creation of condition securing lack of name in User class (Optional package)

--- src/main/java/_f_classes/Optional/NullPointerException/User.java
+++ src/main/java/_f_classes/Optional/NullPointerException/User.java
@@ -2,9 +2,15 @@ package _f_classes.Optional.NullPointerException;
 
 public class User {
 
+    private static final String DEFAULT_PASSWORD = "asdfg12345";
+    private String login;
+    private String password;
     private String name;
 
     public User(String name){
+        if(StringUtils.isEmpty(name)){
+            throw new IllegalArgumentException("Name is a mandatory field to be provided");
+        }
         this.name = name;
     }
 
